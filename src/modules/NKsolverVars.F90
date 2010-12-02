611d81dc2b360ed401541552a9d2a50a635daa09
kenway <kenway@localhost>
2010-12-02 17:27:37 -0500

    added more files for NK solver

---
+++ src/modules/NKsolverVars.F90
@@ -0,0 +1,84 @@
+!
+!     ******************************************************************
+!     *                                                                *
+!     * File:          NKsolverVars.F90                                *
+!     * Author:        Gaetan K.W. Kenway                              *
+!     * Starting date: 12-01-2010                                      *
+!     * Last modified: 12-01-2010                                      *
+!     *                                                                *
+!     ******************************************************************
+!
+module NKsolverVars
+  !
+  !     ******************************************************************
+  !     *                                                                *
+  !     * This module contains the variables needed by the Newton-Krylov *
+  !     * solver.                                                        *
+  !     *                                                                *
+  !     ******************************************************************
+  !
+  use constants
+  implicit none
+
+  ! Non-linear Solver Options
+  integer(kind=intType) :: jacobian_lag
+
+  ! Non-linear Solver Tolerances
+  real(kind=realType) :: snes_atol 
+  real(kind=realType) :: snes_rtol
+  real(kind=realType) :: snes_stol
+  integer(kind=intType) :: snes_max_its
+  integer(kind=intType) :: snes_max_funcs
+
+  ! Krylov-Solver Options
+  character(maxStringLen) :: ksp_solver_type
+  integer(kind=intType) :: ksp_subspace
+  integer(kind=intType) :: asm_overlap
+  character(maxStringLen):: global_pc_type
+  character(maxStringLen):: global_pc_side
+  character(maxStringLen) :: local_pc_type
+  character(maxStringLen) :: local_pc_ordering
+  integer(kind=intType)   :: local_pc_ilu_level
+
+  ! Krylov-Solver Tolerances
+  real(kind=realType) :: ksp_atol
+  real(kind=realType) :: ksp_rtol
+  integer(kind=intTYpe) :: ksp_max_it
+  real(kind=realType) :: ksp_div_tol = 10
+
+  ! Parameter from switching from RK to NK
+  real(kind=realType) :: NK_switch_tol
+
+  ! Define some of the named constants for PETSc 
+
+  ! KSP Types
+  character, parameter :: ksp_gmres = "gmres"
+  character, parameter :: ksP_fgmres = "fgmres"
+  character, parameter :: ksp_bicgstab = "bicgstab"
+
+  ! Global PC Types
+  character, parameter :: pc_blockjacobi = "bjacobi"
+  character, parameter :: pc_jacobi = "jacobi"
+  character, parameter :: pc_asm    = "asm"
+
+  ! Local PC Types
+  character, parameter :: pc_ilu = "ilu"
+  character, parameter :: pc_lu  = "lu"
+  
+  ! Local Orderings
+  character, parameter :: ord_natural = "natural"
+  character, parameter :: ord_rcm     = "rcm"
+  character, parameter :: ord_nd      = "nd"
+  character, parameter :: ord_owd     = "owd"
+
+  ! PC Side
+  character, parameter :: side_left = "left"
+  character, parameter :: side_right = "right"
+  
+  ! Misc Parameters
+  real(kind=realType) :: totalRes0
+  real(kind=realType) :: rhoRes0
+
+  integer(kind=intType) :: iterTot0
+
+end module NKsolverVars
