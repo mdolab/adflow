7cf9317c2843bc896057e9acccd2b0eae7012670
Charles (Sandy) Mader <charles.a.mader@gmail.com>
2011-02-28 14:14:39 -0500

    update stability derivatives

--- src/adjoint/stabilityInput/verifyTSStabilityDerivAdj.f90
+++ src/adjoint/stabilityInput/verifyTSStabilityDerivAdj.f90
@@ -175,200 +175,33 @@ real(kind=realType), dimension(3) :: cfpadjout, cmpadjout
 !     *                                                                *
 !     ******************************************************************
 !
-      dcldalpha = 0.0
-      dcmzdalpha = 0.0
-      call computeTSDerivatives(cl0,cmz0,dcldalpha,dcmzdalpha)
       
-!
-!     ******************************************************************
-!     *                                                                *
-!     * Compute the forces using the tapenade routines.                *
-!     *                                                                *
-!     ******************************************************************
-        
-!     ******************************************************************
-!     * From forcesAndMoments.f90                                      *
-!     * Determine the reference point for the moment computation in    *
-!     * meters.                                                        *
-!     ******************************************************************
- !     print *,' starting Tapenade routines'
-      refPoint(1) = LRef*pointRef(1)
-      refPoint(2) = LRef*pointRef(2)
-      refPoint(3) = LRef*pointRef(3)
-
-      ! Initialize the force and moment coefficients to 0 as well as
-      ! yplusMax.
-     
-      ClAdj  = zero
-      CDAdj  = zero
-      CmxAdj = zero
-      CmyAdj = zero
-      CmzAdj = zero
-      CfxAdj = zero
-      CfyAdj = zero
-      CfzAdj = zero
-
-      yplusMax = zero
-
-      spectralLoopAdj: do sps=1,nTimeIntervalsSpectral
-
-         !zero the force components
-         cFpAdj(1) = zero; cFpAdj(2) = zero; cFpAdj(3) = zero
-         cFvAdj(1) = zero; cFvAdj(2) = zero; cFvAdj(3) = zero
-         cMpAdj(1) = zero; cMpAdj(2) = zero; cMpAdj(3) = zero
-         cMvAdj(1) = zero; cMvAdj(2) = zero; cMvAdj(3) = zero
-         
-
-         ! Loop over the number of local blocks.
-         
-         domainForcesLoop: do nn=1,nDom
-         !   print *,'setting pointers'
-            ! Set some pointers to make the code more readable.
-            call setPointersAdj(nn,groundlevel,sps)
-          !  print *,'allocating'
-            allocate(xAdj(0:ie,0:je,0:ke,3), stat=ierr)
-            if(ierr /= 0)                              &
-                 call terminate("Memory allocation failure for xAdj.")
-
-  
-            allocate(wAdj(0:ib,0:jb,0:kb,nw), stat=ierr)
-            if(ierr /= 0)                              &
-                 call terminate("Memory allocation failure for wAdj.")
-
-            allocate(pAdj(0:ib,0:jb,0:kb), stat=ierr)
-            if(ierr /= 0)                              &
-                 call terminate("Memory allocation failure for pAdj.")
-
-            righthanded = flowDoms(nn,level,sps)%righthanded
-
-          !  print *,'copying stencil'
-            ! Copy the coordinates into xAdj and
-            ! Compute the face normals on the subfaces
-            call  copyADjointForcesStencil(wAdj,xAdj,alphaAdj,betaAdj,&
-           MachAdj,machCoefAdj,machGridAdj,prefAdj,rhorefAdj, pinfdimAdj,&
-           rhoinfdimAdj,rhoinfAdj, pinfAdj,rotRateAdj,rotCenterAdj,murefAdj,&
-           timerefAdj,pInfCorrAdj,nn,level,sps,liftIndex)
-
-            !call copyADjointForcesStencil(wAdj,xAdj,nn,level,sps)
-
-            bocoLoop: do mm=1,nBocos
-               ! Check if this BC needs force integration (Either EulerWall or NSWall)
-
-!s               invForce: if(BCType(mm) == EulerWall        .or. &
-!s                            BCType(mm) == NSWallAdiabatic .or.  &
-!s                            BCType(mm) == NSWallIsothermal) then
-           !    print *,'setting indicies'
-                  ! Determine the range of cell indices of the owned cells
-                  ! Notice these are not the node indices
-                  iiBeg = BCData(mm)%icBeg
-                  iiEnd = BCData(mm)%icEnd
-                  jjBeg = BCData(mm)%jcBeg
-                  jjEnd = BCData(mm)%jcEnd
-
-                  i2Beg= BCData(mm)%inBeg+1; i2End = BCData(mm)%inEnd
-                  j2Beg= BCData(mm)%jnBeg+1; j2End = BCData(mm)%jnEnd
-
-
-!                  allocate(normAdj(iiBeg:iiEnd,jjBeg:jjEnd,3), stat=ierr)
-!                  ! Allocate the memory for the full block stencil
- 
-!                  allocate(siAdj(1:2,iiBeg:iiEnd,jjBeg:jjEnd,3), stat=ierr)
-!                  if(ierr /= 0)                              &
-!                       call terminate("Memory allocation failure for siAdj.")
-                  
-!                  allocate(sjAdj(iiBeg:iiEnd,1:2,jjBeg:jjEnd,3), stat=ierr)
-!                  if(ierr /= 0)                              &
-!                       call terminate("Memory allocation failure for sjAdj.")
-                  
-!                  allocate(skAdj(iiBeg:iiEnd,jjBeg:jjEnd,1:2,3), stat=ierr)
-!                  if(ierr /= 0)                              &
-!                       call terminate("Memory allocation failure for skAdj.")
-                  
-
-                  t = timeUnsteadyRestart
-                  
-                  if(equationMode == timeSpectral) then
-                     do nnn=1,nSections
-                        !t(nnn) = t(nnn) + (sps2-1)*sections(nnn)%timePeriod &
-                        !     /         real(nTimeIntervalsSpectral,realType)
-                        t(nnn) = t(nnn) + (sps-1)*sections(nnn)%timePeriod &
-                             /         (nTimeIntervalsSpectral*1.0)!to make denomenator a real number...
-                     enddo
-                  endif
-               !   print *,'calling computeforces',mm,nn
-
-                  call computeForcesAdj(xAdj,wAdj,pAdj, &
-                       iiBeg,iiEnd,jjBeg,jjEnd,i2Beg,i2End,j2Beg,j2End, &
-                       mm,cFxAdj,cFyAdj,cFzAdj,cMxAdj,cMyAdj,cMzAdj,&
-                       yplusMax,refPoint,CLAdj,CDAdj,  &
-                       nn,level,sps,cFpAdj,cMpAdj,righthanded,secondhalo,&
-                       alphaAdj,betaAdj,machAdj,machcoefAdj,machGridAdj,&
-                       prefAdj,rhorefAdj, pinfdimAdj, rhoinfdimAdj,&
-                       rhoinfAdj, pinfAdj,murefAdj, timerefAdj,pInfCorrAdj,&
-                       rotCenterAdj, rotRateAdj,liftIndex,t)
-                  !print *,'forces computed'
-                  !print *,'cfpadj',cfpadj,cmpadj,'cladj',cladj,'cdadj',cdadj
-
-
-!                  deallocate(normAdj,siAdj,sjAdj,skAdj)
-!               end if invForce
-
-
-            end do bocoLoop
-!            print *,'deallocating p'
-            deallocate(pAdj)
-!            print *,'deallocating w'
-            deallocate(wAdj)
- !           print *,'deallocating x'
-            deallocate(xAdj)
-  !          print *,'deallocation finished'
-!            deallocate(wAdj)
+      call computeTSDerivatives(coef0,dcdalpha,dcdalphadot,dcdq,dcdqdot)
       
-
-         end do domainForcesLoop
-     !    print *,'finished domain loop'
-!write(*,'(a5,2i5,7g)')'adj',myID,sps,ClAdj(sps),cfpadjout(1),cfpadjout(2),cfpadjout(3),cmpadjout(1),cmpadjout(2),cmpadjout(3)      
-         monLoc2(1) = CLAdj(sps)
-         monLoc2(2) = CDAdj(sps)
-         monLoc2(3) = CfxAdj(sps)
-         monLoc2(4) = CfyAdj(sps)
-         monLoc2(5) = CfzAdj(sps)
-         monLoc2(6) = CMxAdj(sps)
-         monLoc2(7) = CMyAdj(sps)
-         monLoc2(8) = CMzAdj(sps)
-         
-         ! Determine the global sum of the summation monitoring
-         ! variables. The sum is made known to all processors.
-!         write(*,*)'adj ',myID,monLoc2(1)
-     !    write(*,*)'nmonsum',nMonSum
-         call mpi_allreduce(monLoc2, monGlob2, nMonSum2, sumb_real, &
-              mpi_sum, SUmb_comm_world, ierr)
-     !    print *,'reduction complete'
-         ! Transfer the cost function values to output arguments.
-     !     print *,'assigning results'
-         CLAdj(sps)  = monGlob2(1)
-         CDAdj(sps)  = monGlob2(2)
-         CfxAdj(sps) = monGlob2(3)
-         CfyAdj(sps) = monGlob2(4)
-         CfzAdj(sps) = monGlob2(5) 
-         CMxAdj(sps) = monGlob2(6)
-         CMyAdj(sps) = monGlob2(7)
-         CMzAdj(sps) = monGlob2(8)
-
-      end do spectralLoopAdj
-
       call mpi_barrier(SUmb_comm_world, ierr)
 
+      do sps =1,nTimeIntervalsSpectral
+         
+         level = 1
+         call computeAeroCoef(globalCFVals,sps)
+         
+         BaseCoef(sps,1) = globalCFVals(costFuncLiftCoef)
+         BaseCoef(sps,2) = globalCFVals(costFuncDragCoef)
+         BaseCoef(sps,3) = globalCFVals(costFuncForceXCoef)
+         BaseCoef(sps,4) = globalCFVals(costFuncForceYCoef)
+         BaseCoef(sps,5) = globalCFVals(costFuncForceZCoef)
+         BaseCoef(sps,6) = globalCFVals(costFuncMomXCoef)
+         BaseCoef(sps,7) = globalCFVals(costFuncMomYCoef)
+         BaseCoef(sps,8) = globalCFVals(costFuncMomZCoef)
+         
+         
+      end do
 
-      cl0Adj =0.0
-      cmz0Adj =0.0
-      dcldalphaAdj =0.0
-      dcmzdalphaAdj =0.0
+        
       !Now compute the stability derivatives
 
-      call computeTSStabilityDerivAdj(cFxAdj,cFyAdj,cFzAdj,cMxAdj,&
-                       cMyAdj,cMzAdj,CLAdj,CDAdj,&
-                       cl0Adj,cmz0Adj,dcldalphaAdj,dcmzdalphaAdj)
+      call computeTSStabilityDerivAdj(BaseCoef,coef0,dcdalpha,&
+           dcdalphadot,dcdq,dcdqdot)
 
 
       ! Root processor outputs results.
