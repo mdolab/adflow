eb679701aa9a8c441604f150ad189833fa613cf1
kenway <kenway@localhost>
2011-09-20 13:06:02 -0400

    added a missing file

---
+++ src/partitioning/checkPartitioning.f90
@@ -0,0 +1,35 @@
+subroutine checkPartitioning(np,load_inbalance,face_inbalance)
+
+  ! This subroutine runs the load balancing and partitioning algorithm
+  ! to determine what the load balancing will be for a given number of
+  ! procs np. The output is load_inbalance and face_inbalance. 
+
+  use communication
+  use blockPointers
+  use cgnsGrid
+  use inputIO
+  use IOModule
+  use partitionMod
+  implicit none
+ 
+  integer(kind=intType),intent(in) ::np
+  integer(kind=intType) :: nproc_save
+  real(kind=realType),intent(out) :: load_inbalance,face_inbalance
+
+  ! Note: This file follows mostly partitionAndReadGrid. See
+  ! partitionAndReadGrid.f90 for more infromation
+  
+  ! Trick it into thinking we have np processors:
+  nproc_save = nproc
+  nproc = np
+  
+  call blockDistribution
+  
+  ! Restore the number of procs
+  nproc = nproc_save
+  
+  ! Extract the inbalance info:
+  load_inbalance = ubvec(1)
+  face_inbalance = ubvec(2)
+
+end subroutine checkPartitioning
