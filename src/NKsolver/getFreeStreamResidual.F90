e5ea93fda7f7b62d056e77abea57e7fbd8b7f3ad
Gaetan Kenway <gaetank@gmail.com>
2011-11-29 15:31:33 -0500

    fixed a bug when computing freestream residuals at beginning of calculation. Also cleaned up some comments

--- src/NKsolver/getFreeStreamResidual.F90
+++ src/NKsolver/getFreeStreamResidual.F90
@@ -11,9 +11,9 @@ subroutine getFreeStreamResidual(rhoRes,totalRRes)
   implicit none
 
   real(kind=realType), intent(out) :: rhoRes,totalRRes
-  real(kind=realType),dimension(:), allocatable :: wtemp
-  real(kind=realType) :: temp1,temp2
-  integer(kind=intType) :: nDimW,ierr,counter
+  real(kind=realType),dimension(:), allocatable :: wtemp,ptemp
+  real(kind=realType) :: temp1,temp2,v2,gm1
+  integer(kind=intType) :: nDimW,nDimP,ierr,counter
   integer(kind=intType) :: tempStartLevel,tempCurrentLevel,tempMGStartLevel
   integer(kind=intType) :: nn,sps,i,j,k,l
 
@@ -21,26 +21,55 @@ subroutine getFreeStreamResidual(rhoRes,totalRRes)
 
   ! We need to copy the current wvector temporirly since it may be a
   ! restart and actually useful
-  nDimW = nw * nCellsLocal * nTimeIntervalsSpectral
-  allocate(wtemp(nDimW))
+
+  ! Copy out ALL states and ALL pressures including the halos. 
+  nDimW = 0
+  nDimP = 0
+  do nn=1,nDom
+     call setPointers(nn,1,1)
+     nDimp = nDimp + (ib+1)*(jb+1)*(kb+1)
+     nDimW = nDimw + (ib+1)*(jb+1)*(kb+1)
+  end do
+
+  nDimp = nDimp * nTimeIntervalsSpectral
+  nDimw = nDimw * nTimeIntervalsSpectral * nw 
+
+  allocate(wtemp(nDimW),ptemp(nDimP))
 
   ! Copy w to wTemp
   counter = 0
-  spectralLoop: do sps=1,nTimeIntervalsSpectral
-     domains: do nn=1,nDom
+  spectralLoop1: do sps=1,nTimeIntervalsSpectral
+     domains1: do nn=1,nDom
         call setPointers(nn,1,sps)
         do l=1,nw
-           do k=2,kl
-              do j=2,jl
-                 do i=2,il
+           do k=0,kb
+              do j=0,jb
+                 do i=0,ib
                     counter = counter + 1
                     wtemp(counter) = w(i,j,k,l)
                  enddo
               enddo
            enddo
         enddo
-     end do domains
-  end do spectralLoop
+     end do domains1
+  end do spectralLoop1
+
+  ! Copy p to pTemp
+  counter = 0
+  spectralLoop2: do sps=1,nTimeIntervalsSpectral
+     domains2: do nn=1,nDom
+        call setPointers(nn,1,sps)
+        do k=0,kb
+           do j=0,jb
+              do i=0,ib
+                 counter = counter + 1
+                 ptemp(counter) = p(i,j,k)
+              enddo
+           enddo
+        enddo
+     end do domains2
+  end do spectralLoop2
+
   
   tempMGStartLevel = mgStartLevel
   tempCurrentLevel = currentLevel
@@ -49,30 +78,46 @@ subroutine getFreeStreamResidual(rhoRes,totalRRes)
   mgStartLevel = 1
   currentlevel = 1
   !startLevel   = 1
-
+  
   call setUniformFlow
   call getCurrentResidual(rhoRes,totalRRes)
+
   counter = 0
-  spectralLoop2: do sps=1,nTimeIntervalsSpectral
-     domains2: do nn=1,nDom
+  spectralLoop3: do sps=1,nTimeIntervalsSpectral
+     domains3: do nn=1,nDom
         call setPointers(nn,1,sps)
         do l=1,nw
-           do k=2,kl
-              do j=2,jl
-                 do i=2,il
+           do k=0,kb
+              do j=0,jb
+                 do i=0,ib
                     counter = counter + 1
                     w(i,j,k,l) = wtemp(counter) 
                  enddo
               enddo
            enddo
         enddo
-     end do domains2
-  end do spectralLoop2
+     end do domains3
+  end do spectralLoop3
+
+  counter = 0
+  spectralLoop4: do sps=1,nTimeIntervalsSpectral
+     domains4: do nn=1,nDom
+        call setPointers(nn,1,sps)
+        do k=0,kb
+           do j=0,jb
+              do i=0,ib
+                 counter = counter + 1
+                 p(i,j,k) = ptemp(counter) 
+              enddo
+           enddo
+        enddo
+     end do domains4
+  end do spectralLoop4
 
   mgStartLevel = tempMGStartLevel
   currentLevel = tempCurrentLevel
   !startLevel   = tempStartLevel
 
-  deallocate(wtemp)
+  deallocate(wtemp,ptemp)
 
 end subroutine getFreeStreamResidual
