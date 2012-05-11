23b261ad27d19f303229598bc66ec80f73fcc6ed
Gaetan Kenway <gaetank@gmail.com>
2012-05-11 14:37:45 -0400

    removed bad print statement

--- src/wallDistance/wallDistance.f90
+++ src/wallDistance/wallDistance.f90
@@ -61,8 +61,6 @@
        ! distance. It may be needed for the boundary conditions and
        ! the monitoring of the y+. Return afterwards.
 
-       print *,'use is:',useapproxwalldistance
-
        if(.not. wallDistanceNeeded) then
 
          ! Loop over the number of spectral solutions, initialize the
