eb0cf9cb9a21405266e7dc2e84be1de633395981
Gaetan Kenway <gaetank@gmail.com>
2013-09-24 13:19:08 -0400

    added class imports to __init__. This more closely matches how mdo_import_helper did it

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 import sys, os
 cur_dir = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(cur_dir, 'python')
-import pySUmb, pySUmb_C
-
-__all__ = ['pySUmb','pySUmb_C']
+sys.path.append(os.path.join(cur_dir, 'python'))
+import pySUmb
+import pySUmb_C
+from pySUmb import SUMB
+from pySUmb_C import SUMB_C
+__all__ = ['pySUmb', 'pySUmb_C', 'SUMB', 'SUMB_C']
