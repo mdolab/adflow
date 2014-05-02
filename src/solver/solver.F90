ec1ff59f9c33900d39d7642c221aa5db86e678d9
kenway <kenway@localhost>
2014-05-02 07:46:21 -0700

    fixed a bug in the lift dist bounds calculation.

--- src/solver/solver.F90
+++ src/solver/solver.F90
@@ -100,9 +100,9 @@
          select case (equationMode)
            case (steady, timeSpectral)
              call solverSteady
-             if (routineFailed) then
-                exit
-             end if
+             ! if (fatalFailroutineFailed) then
+             !     exit
+             !  end if
            case (unsteady)
              select case (timeIntegrationScheme)
                case (BDF)
