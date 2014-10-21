7da3f531a3bc37bf5db12e5f0b45dc0dbcba2c1d
lvzhoujie <lvzhoujie@localhost>
2014-10-21 14:04:00 -0400

    add resetgamma

--- src/utils/resetgammaBwd.f90
+++ src/utils/resetgammaBwd.f90
@@ -1,14 +1,14 @@
 !
 !      ******************************************************************
 !      *                                                                *
-!      * File:          setgammaBwd.f90                                 *
+!      * File:          resetgammaBwd.f90                               *
 !      * Author:        Peter Zhoujie Lyu                               *
-!      * Starting date: 10-15-2014                                      *
+!      * Starting date: 10-21-2014                                      *
 !      * Last modified: 10-21-2014                                      *
 !      *                                                                *
 !      ******************************************************************
 !
-subroutine setgammaBwd(nn, gamma1, gamma2)
+subroutine resetgammaBwd(nn, gamma1, gamma2)
 
   use BCTypes
   use blockPointers
@@ -32,24 +32,24 @@ subroutine setgammaBwd(nn, gamma1, gamma2)
 
   select case (BCFaceID(nn))
   case (iMin)
-     gamma1(1:je,1:ke) = gamma(1, 1:je,1:ke)
-     gamma2(1:je,1:ke) = gamma(2, 1:je,1:ke)
+      gamma(1, 1:je,1:ke) = gamma1(1:je,1:ke)
+      gamma(2, 1:je,1:ke) = gamma2(1:je,1:ke)
   case (iMax)
-     gamma1(1:je,1:ke) = gamma(ie,1:je,1:ke)
-     gamma2(1:je,1:ke) = gamma(il,1:je,1:ke)
+      gamma(ie,1:je,1:ke) = gamma1(1:je,1:ke)
+      gamma(il,1:je,1:ke) = gamma2(1:je,1:ke)
   case (jMin)
-     gamma1(1:ie,1:ke) = gamma(1:ie,1, 1:ke)
-     gamma2(1:ie,1:ke) = gamma(1:ie,2, 1:ke)
+      gamma(1:ie,1, 1:ke) = gamma1(1:ie,1:ke)
+      gamma(1:ie,2, 1:ke) = gamma2(1:ie,1:ke)
   case (jMax)
-     gamma1(1:ie,1:ke) = gamma(1:ie,je,1:ke)
-     gamma2(1:ie,1:ke) = gamma(1:ie,jl,1:ke)
+      gamma(1:ie,je,1:ke) = gamma1(1:ie,1:ke)
+      gamma(1:ie,jl,1:ke) = gamma2(1:ie,1:ke)
   case (kMin)
-     gamma1(1:ie,1:je) = gamma(1:ie,1:je,1 ) 
-     gamma2(1:ie,1:je) = gamma(1:ie,1:je,2 )
+      gamma(1:ie,1:je,1 ) = gamma1(1:ie,1:je)
+      gamma(1:ie,1:je,2 ) = gamma2(1:ie,1:je)
   case (kMax)
-     gamma1(1:ie,1:je) = gamma(1:ie,1:je,ke) 
-     gamma2(1:ie,1:je) = gamma(1:ie,1:je,kl)
+      gamma(1:ie,1:je,ke) = gamma1(1:ie,1:je)
+      gamma(1:ie,1:je,kl) = gamma2(1:ie,1:je)
   end select
 
-end subroutine setgammaBwd
+end subroutine resetgammaBwd
 
