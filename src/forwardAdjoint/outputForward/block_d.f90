8d8310ea50707b48ea6e9af6d0b5b92fa99d95de
Peter Lyu <lvzhoujie@gmail.com>
2014-10-31 11:42:51 -0400

    reAD forward for the caviation

--- src/forwardAdjoint/outputForward/block_d.f90
+++ src/forwardAdjoint/outputForward/block_d.f90
@@ -144,6 +144,7 @@
    REAL(kind=realtype), DIMENSION(:, :, :), POINTER :: m
    REAL(kind=realtype), DIMENSION(:, :), POINTER :: oarea
    REAL(kind=realtype), DIMENSION(:, :), POINTER :: sepsensor
+   REAL(kind=realtype), DIMENSION(:, :), POINTER :: cavitation
    REAL(kind=realtype), DIMENSION(3) :: symnorm
    REAL(kind=realtype), DIMENSION(:, :, :), POINTER :: uslip
    END TYPE BCDATATYPE_D
