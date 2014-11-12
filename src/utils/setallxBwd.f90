28ae81de98fc2df7f5c2c1dde17339f7a3e2572d
Peter Lyu <lvzhoujie@gmail.com>
2014-11-12 14:58:53 -0500

    correct x fake pointers which fixed a off by one error

--- src/utils/setallxBwd.f90
+++ src/utils/setallxBwd.f90
@@ -18,7 +18,7 @@ subroutine setallxBwd(nn, x0, x1, x2)
   !      Subroutine arguments.
   !
   integer(kind=intType), intent(in) :: nn
-  real(kind=realType), dimension(0:imaxDim,0:jmaxDim,3) :: x0, x1, x2
+  real(kind=realType), dimension(imaxDim+1,jmaxDim+1,3) :: x0, x1, x2
   !
   !      ******************************************************************
   !      *                                                                *
@@ -35,44 +35,44 @@ subroutine setallxBwd(nn, x0, x1, x2)
   select case (BCFaceID(nn))
 
   case (iMin)
-     x0(0:je,0:ke,:) = x(0,0:je,0:ke,:)
-     x1(0:je,0:ke,:) = x(1,0:je,0:ke,:)
-     x2(0:je,0:ke,:) = x(2,0:je,0:ke,:)
+     x0(1:je+1,1:ke+1,:) = x(0,0:je,0:ke,:)
+     x1(1:je+1,1:ke+1,:) = x(1,0:je,0:ke,:)
+     x2(1:je+1,1:ke+1,:) = x(2,0:je,0:ke,:)
 
      !===========================================================
 
   case (iMax)
-     x0(0:je,0:ke,:) = x(ie,0:je,0:ke,:)
-     x1(0:je,0:ke,:) = x(il,0:je,0:ke,:)
-     x2(0:je,0:ke,:) = x(nx,0:je,0:ke,:)
+     x0(1:je+1,1:ke+1,:) = x(ie,0:je,0:ke,:)
+     x1(1:je+1,1:ke+1,:) = x(il,0:je,0:ke,:)
+     x2(1:je+1,1:ke+1,:) = x(nx,0:je,0:ke,:)
 
      !===========================================================
 
   case (jMin)
-     x0(0:ie,0:ke,:) = x(0:ie,0,0:ke,:)
-     x1(0:ie,0:ke,:) = x(0:ie,1,0:ke,:)
-     x2(0:ie,0:ke,:) = x(0:ie,2,0:ke,:)
+     x0(1:ie+1,1:ke+1,:) = x(0:ie,0,0:ke,:)
+     x1(1:ie+1,1:ke+1,:) = x(0:ie,1,0:ke,:)
+     x2(1:ie+1,1:ke+1,:) = x(0:ie,2,0:ke,:)
  
      !===========================================================
 
   case (jMax)
-     x0(0:ie,0:ke,:) = x(0:ie,je,0:ke,:)
-     x1(0:ie,0:ke,:) = x(0:ie,jl,0:ke,:)
-     x2(0:ie,0:ke,:) = x(0:ie,ny,0:ke,:)
+     x0(1:ie+1,1:ke+1,:) = x(0:ie,je,0:ke,:)
+     x1(1:ie+1,1:ke+1,:) = x(0:ie,jl,0:ke,:)
+     x2(1:ie+1,1:ke+1,:) = x(0:ie,ny,0:ke,:)
  
      !===========================================================
 
   case (kMin)
-     x0(0:ie,0:je,:) = x(0:ie,0:je,0,:)
-     x1(0:ie,0:je,:) = x(0:ie,0:je,1,:)
-     x2(0:ie,0:je,:) = x(0:ie,0:je,2,:)
+     x0(1:ie+1,1:je+1,:) = x(0:ie,0:je,0,:)
+     x1(1:ie+1,1:je+1,:) = x(0:ie,0:je,1,:)
+     x2(1:ie+1,1:je+1,:) = x(0:ie,0:je,2,:)
 
      !===========================================================
 
   case (kMax)
-     x0(0:ie,0:je,:) = x(0:ie,0:je,ke,:)
-     x1(0:ie,0:je,:) = x(0:ie,0:je,kl,:)
-     x2(0:ie,0:je,:) = x(0:ie,0:je,nz,:)
+     x0(1:ie+1,1:je+1,:) = x(0:ie,0:je,ke,:)
+     x1(1:ie+1,1:je+1,:) = x(0:ie,0:je,kl,:)
+     x2(1:ie+1,1:je+1,:) = x(0:ie,0:je,nz,:)
 
   end select
 end subroutine setallxBwd
