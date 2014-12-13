6cc2be92eaf64225b1cc63dc06a3ed1741a372aa
kenway <kenway@localhost>
2014-12-13 18:32:22 -0500

    Modified more turbulence files to remove pointers

--- src/turbulence/turbBCNSWall.f90
+++ src/turbulence/turbBCNSWall.f90
@@ -8,159 +8,169 @@
 !      *                                                                *
 !      ******************************************************************
 !
-       subroutine turbBCNSWall(secondHalo)
-!
-!      ******************************************************************
-!      *                                                                *
-!      * turbBCNSWall applies the viscous wall boundary conditions      *
-!      * of the turbulent transport equations to a block. It is assumed *
-!      * that the pointers in blockPointers are already set to the      *
-!      * correct block on the correct grid level.                       *
-!      *                                                                *
-!      ******************************************************************
-!
-       use BCTypes
-       use blockPointers
-       use flowVarRefState
-       implicit none
-!
-!      Subroutine argument.
-!
-       logical, intent(in) :: secondHalo
-!
-!      Local variables.
-!
-       integer(kind=intType) :: nn, i, j, l, m
+subroutine turbBCNSWall(secondHalo)
+  !
+  !      ******************************************************************
+  !      *                                                                *
+  !      * turbBCNSWall applies the viscous wall boundary conditions      *
+  !      * of the turbulent transport equations to a block. It is assumed *
+  !      * that the pointers in blockPointers are already set to the      *
+  !      * correct block on the correct grid level.                       *
+  !      *                                                                *
+  !      ******************************************************************
+  !
+  use BCTypes
+  use blockPointers
+  use flowVarRefState
+  implicit none
+  !
+  !      Subroutine argument.
+  !
+  logical, intent(in) :: secondHalo
+  !
+  !      Local variables.
+  !
+  integer(kind=intType) :: nn, i, j, l, m
 
-       real(kind=realType), dimension(:,:,:,:), pointer :: bmt
-       real(kind=realType), dimension(:,:,:), pointer :: bvt
-       real(kind=realType), dimension(:,:,:), pointer :: ww0, ww1, ww2
-       real(kind=realType), dimension(:,:), pointer :: rev0, rev1, rev2
-!
-!      ******************************************************************
-!      *                                                                *
-!      * Begin execution                                                *
-!      *                                                                *
-!      ******************************************************************
-!
-       ! Loop over the viscous subfaces of this block.
+  !
+  !      ******************************************************************
+  !      *                                                                *
+  !      * Begin execution                                                *
+  !      *                                                                *
+  !      ******************************************************************
+  !
+  ! Loop over the viscous subfaces of this block.
 
-       bocos: do nn=1,nViscBocos
+  bocos: do nn=1,nViscBocos
 
-         ! Set the corresponding arrays.
+     ! Set the corresponding arrays.
 
-         call BCTurbWall(nn)
+     call BCTurbWall(nn)
 
-         ! Determine the block face on which this subface is located
-         ! and set some pointers accordingly.
+     ! Loop over the faces and set the state in
+     ! the turbulent halo cells.
 
-         select case (BCFaceID(nn))
-           case (iMin)
-             bmt => bmti1; bvt => bvti1
-             ww0 => w(0 ,1:,1:,:); ww1 => w(1 ,1:,1:,:)
-             ww2 => w(2 ,1:,1:,:)
-
-             if( eddyModel ) then
-               rev0 => rev(0 ,1:,1:); rev1 => rev(1 ,1:,1:)
-               rev2 => rev(2 ,1:,1:)
-             endif
-
-           case (iMax)
-             bmt => bmti2; bvt => bvti2
-             ww0 => w(ib,1:,1:,:); ww1 => w(ie,1:,1:,:)
-             ww2 => w(il,1:,1:,:)
-
-             if( eddyModel ) then
-               rev0 => rev(ib,1:,1:); rev1 => rev(ie,1:,1:)
-               rev2 => rev(il,1:,1:)
-             endif
-
-           case (jMin)
-             bmt => bmtj1; bvt => bvtj1
-             ww0 => w(1:,0 ,1:,:); ww1 => w(1:,1 ,1:,:)
-             ww2 => w(1:,2 ,1:,:)
-
-             if( eddyModel ) then
-               rev0 => rev(1:,0 ,1:); rev1 => rev(1:,1 ,1:)
-               rev2 => rev(1:,2 ,1:)
-             endif
-
-           case (jMax)
-             bmt => bmtj2; bvt => bvtj2
-             ww0 => w(1:,jb,1:,:); ww1 => w(1:,je,1:,:)
-             ww2 => w(1:,jl,1:,:)
-
-             if( eddyModel ) then
-               rev0 => rev(1:,jb,1:); rev1 => rev(1:,je,1:)
-               rev2 => rev(1:,jl,1:)
-             endif
-
-           case (kMin)
-             bmt => bmtk1; bvt => bvtk1
-             ww0 => w(1:,1:,0 ,:); ww1 => w(1:,1:,1 ,:)
-             ww2 => w(1:,1:,2 ,:)
-
-             if( eddyModel ) then
-               rev0 => rev(1:,1:, 0); rev1 => rev(1:,1:, 1)
-               rev2 => rev(1:,1:, 2)
-             endif
-
-           case (kMax)
-             bmt => bmtk2; bvt => bvtk2
-             ww0 => w(1:,1:,kb,:); ww1 => w(1:,1:,ke,:)
-             ww2 => w(1:,1:,kl,:)
-
-             if( eddyModel ) then
-               rev0 => rev(1:,1:,kb); rev1 => rev(1:,1:,ke)
-               rev2 => rev(1:,1:,kl)
-             endif
-         end select
-
-         ! Loop over the faces and set the state in
-         ! the turbulent halo cells.
-
-         do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+     select case (BCFaceID(nn))
+     case (iMin)
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
            do i=BCData(nn)%icBeg, BCData(nn)%icEnd
-             do l=nt1,nt2
-               ww1(i,j,l) = bvt(i,j,l)
-               do m=nt1,nt2
-                 ww1(i,j,l) = ww1(i,j,l) - bmt(i,j,l,m)*ww2(i,j,m)
-               enddo
-             enddo
-           enddo
-         enddo
 
-         ! Use constant extrapolation if the state in the second halo
-         ! must be computed.
+              do l=nt1,nt2
+                 w(1,i,j,l) = bvti1(i,j,l)
+                 do m=nt1,nt2
+                    w(1,i,j,l) = w(1,i,j,l) - bmti1(i,j,l,m)*w(2,i,j,m)
+                 enddo
+                 if (secondHalo) w(0,i,j,l) = w(1,i,j,l)
+              end do
 
-         if( secondHalo ) then
-           do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
-             do i=BCData(nn)%icBeg, BCData(nn)%icEnd
-               do l=nt1,nt2
-                 ww0(i,j,l) = ww1(i,j,l)
-               enddo
-             enddo
-           enddo
-         endif
+              if (eddyModel) then 
+                 rev(1,i,j) = -rev(2,i,j)
+                 if (secondHalo) then 
+                    rev(0,i,j) = rev(1,i,j)
+                 end if
+              end if
+           end do
+        end do
+     case (iMax) 
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+           do i=BCData(nn)%icBeg, BCData(nn)%icEnd
 
-         ! Set the eddy viscosity for an eddy model.
+              do l=nt1,nt2
+                 w(ie,i,j,l) = bvti2(i,j,l)
+                 do m=nt1,nt2
+                    w(ie,i,j,l) = w(ie,i,j,l) - bmti2(i,j,l,m)*w(il,i,j,m)
+                 enddo
+                 if (secondHalo) w(ib,i,j,l) = w(ie,i,j,l)
+              end do
 
-         if( eddyModel ) then
-           do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
-             do i=BCData(nn)%icBeg, BCData(nn)%icEnd
-               rev1(i,j) = -rev2(i,j)
-             enddo
-           enddo
+              if (eddyModel) then 
+                 rev(ie,i,j) = -rev(il,i,j)
+                 if (secondHalo) then 
+                    rev(ib,i,j) = rev(ie,i,j)
+                 end if
+              end if
+           end do
+        end do
+     case (jMin)
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+           do i=BCData(nn)%icBeg, BCData(nn)%icEnd
 
-           if( secondHalo ) then
-             do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
-               do i=BCData(nn)%icBeg, BCData(nn)%icEnd
-                 rev0(i,j) = rev1(i,j)
-               enddo
-             enddo
-           endif
-         endif
+              do l=nt1,nt2
+                 w(i,1,j,l) = bvtj1(i,j,l)
+                 do m=nt1,nt2
+                    w(i,1,j,l) = w(i,1,j,l) - bmtj1(i,j,l,m)*w(i,2,j,m)
+                 enddo
+                 if (secondHalo) w(i,0,j,l) = w(i,1,j,l)
+              end do
 
-       enddo bocos
+              if (eddyModel) then 
+                 rev(i,1,j) = -rev(i,2,j)
+                 if (secondHalo) then 
+                    rev(i,0,j) = rev(i,1,j)
+                 end if
+              end if
+           end do
+        end do
+     case (jMax)
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+           do i=BCData(nn)%icBeg, BCData(nn)%icEnd
 
-       end subroutine turbBCNSWall
+              do l=nt1,nt2
+                 w(i,je,j,l) = bvtj2(i,j,l)
+                 do m=nt1,nt2
+                    w(i,je,j,l) = w(i,je,j,l) - bmtj2(i,j,l,m)*w(i,jl,j,m)
+                 enddo
+                 if (secondHalo) w(i,jb,j,l) = w(i,je,j,l)
+              end do
+
+              if (eddyModel) then 
+                 rev(i,je,j) = -rev(i,jl,j)
+                 if (secondHalo) then 
+                    rev(i,jb,j) = rev(i,je,j)
+                 end if
+              end if
+           end do
+        end do
+     case (kMin)
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+           do i=BCData(nn)%icBeg, BCData(nn)%icEnd
+
+              do l=nt1,nt2
+                 w(i,j,1,l) = bvtk1(i,j,l)
+                 do m=nt1,nt2
+                    w(i,j,1,l) = w(i,j,1,l) - bmtk1(i,j,l,m)*w(i,j,2,m)
+                 enddo
+                 if (secondHalo) w(i,j,0,l) = w(i,j,1,l)
+              end do
+
+              if (eddyModel) then 
+                 rev(i,j,1) = -rev(i,j,2)
+                 if (secondHalo) then 
+                    rev(i,j,0) = rev(i,j,1)
+                 end if
+              end if
+           end do
+        end do
+     case (kMax)
+        do j=BCData(nn)%jcBeg, BCData(nn)%jcEnd
+           do i=BCData(nn)%icBeg, BCData(nn)%icEnd
+
+              do l=nt1,nt2
+                 w(i,j,ke,l) = bvtk2(i,j,l)
+                 do m=nt1,nt2
+                    w(i,j,ke,l) = w(i,j,ke,l) - bmtk2(i,j,l,m)*w(i,j,kl,m)
+                 enddo
+                 if (secondHalo) w(i,j,kb,l) = w(i,j,ke,l)
+              end do
+
+              if (eddyModel) then 
+                 rev(i,j,ke) = -rev(i,j,kl)
+                 if (secondHalo) then 
+                    rev(i,j,kb) = rev(i,j,ke)
+                 end if
+              end if
+           end do
+        end do
+     end select
+  enddo bocos
+end subroutine turbBCNSWall
