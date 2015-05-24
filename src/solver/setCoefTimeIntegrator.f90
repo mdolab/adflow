a373e735b8eb152a64ff68e05c616f64373857ca
dnhuang <dnhuang@umich.edu>
2015-05-24 15:23:29 -0400

    Test merging 1

--- src/solver/setCoefTimeIntegrator.f90
+++ src/solver/setCoefTimeIntegrator.f90
@@ -22,6 +22,7 @@
 !
        use constants
        use inputUnsteady
+       use inputPhysics
        use iteration
        use monitor
        implicit none
@@ -38,6 +39,9 @@
 !
        ! Determine which time integrator must be used.
 
+       ! *******************************
+       ! Modified by HDN
+       ! *******************************
        select case (timeAccuracy)
          case (firstOrder)
 
@@ -48,6 +52,12 @@
            coefTime(0) =  1.0_realType
            coefTime(1) = -1.0_realType
 
+           if ( useALE .and. equationMode .eq. unsteady)  then
+              coefTimeALE(1)   = 1.0_realType
+              coefMeshALE(1,1) = half
+              coefMeshALE(1,2) = half
+           end if
+
            nLevelsSet = 2
 
            !=============================================================
@@ -56,18 +66,43 @@
 
            ! Second order time integrator. Determine the amount of
            ! available states and set the coefficients accordingly.
-
            select case (nOldSolAvail)
 
              case (1_intType)
                coefTime(0) =  1.0_realType
                coefTime(1) = -1.0_realType
+
+               if ( useALE .and. equationMode .eq. unsteady)  then
+                  coefTimeALE(1)   = half
+                  coefTimeALE(2)   = half
+                  coefTimeALE(3)   = zero
+                  coefTimeALE(4)   = zero
+
+                  coefMeshALE(1,1) = half
+                  coefMeshALE(1,2) = half
+                  coefMeshALE(2,1) = half
+                  coefMeshALE(2,2) = half
+               end if
+
                nLevelsSet  = 2
 
              case default   ! 2 or bigger.
                coefTime(0) =  1.5_realType
                coefTime(1) = -2.0_realType
                coefTime(2) =  0.5_realType
+
+               if ( useALE .and. equationMode .eq. unsteady)  then
+                  coefTimeALE(1)   = threefourth
+                  coefTimeALE(2)   = threefourth
+                  coefTimeALE(3)   = -fourth
+                  coefTimeALE(4)   = -fourth
+
+                  coefMeshALE(1,1) = half*(1.0_realType+1.0_realType/sqrtthree)
+                  coefMeshALE(1,2) = half*(1.0_realType-1.0_realType/sqrtthree)
+                  coefMeshALE(2,1) = coefMeshALE(1,2)
+                  coefMeshALE(2,2) = coefMeshALE(1,1)
+               end if
+
                nLevelsSet  = 3
 
            end select
@@ -84,12 +119,29 @@
              case (1_intType)
                coefTime(0) =  1.0_realType
                coefTime(1) = -1.0_realType
+
+               if ( useALE .and. equationMode .eq. unsteady)  then
+                  coefTimeALE(1)   = 1.0_realType
+                  coefMeshALE(1,1) = half
+                  coefMeshALE(1,2) = half
+               end if
+
                nLevelsSet  = 2
 
              case (2_intType)
                coefTime(0) =  1.5_realType
                coefTime(1) = -2.0_realType
                coefTime(2) =  0.5_realType
+
+               if ( useALE .and. equationMode .eq. unsteady)  then
+                  coefTimeALE(1)   = threefourth
+                  coefTimeALE(2)   = -fourth
+                  coefMeshALE(1,1) = half*(1.0_realType+1.0_realType/sqrtthree)
+                  coefMeshALE(1,2) = half*(1.0_realType-1.0_realType/sqrtthree)
+                  coefMeshALE(2,1) = coefMeshALE(1,2)
+                  coefMeshALE(2,2) = coefMeshALE(1,1)
+               end if
+
                nLevelsSet  = 3
 
              case default   ! 3 or bigger.
@@ -97,6 +149,23 @@
                coefTime(1) = -3.0_realType
                coefTime(2) =  1.5_realType
                coefTime(3) = -1.0_realType/3.0_realType
+
+               ! These numbers are NOT correct
+               ! DO NOT use 3rd order ALE for now
+               if ( useALE .and. equationMode .eq. unsteady)  then
+                  print *, 'Third-order ALE not implemented yet.'
+                  coefTimeALE(1)   = threefourth
+                  coefTimeALE(2)   = threefourth
+                  coefTimeALE(3)   = -fourth
+                  coefTimeALE(4)   = -fourth
+                  coefMeshALE(1,1) = half*(1.0_realType+1.0_realType/sqrtthree)
+                  coefMeshALE(1,2) = half*(1.0_realType-1.0_realType/sqrtthree)
+                  coefMeshALE(2,1) = coefMeshALE(1,2)
+                  coefMeshALE(2,2) = coefMeshALE(1,1)
+                  coefMeshALE(3,1) = coefMeshALE(1,2)
+                  coefMeshALE(3,2) = coefMeshALE(1,1)
+               end if
+
                nLevelsSet  = 4
 
            end select
@@ -110,4 +179,4 @@
          coefTime(nn) = zero
        enddo
 
-       end subroutine setCoefTimeIntegrator
+     end subroutine setCoefTimeIntegrator
