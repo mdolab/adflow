bfcb4177ce52072091d2440423f19cb2ec0553db
Eirikur Jonsson <eirikurj@umich.edu>
2015-10-28 14:09:44 -0400

    Making the check for capital letters in the sumb.pyf file more robust. It handles better lines that have comments and code

--- src/python/f2py/checkPyfForUpperCase.py
+++ src/python/f2py/checkPyfForUpperCase.py
@@ -8,27 +8,47 @@ case characters. Text in comments and preprocessor tags are ignored
 
 import sys
 
-f = open("sumb.pyf","r")
+def error(i,line):
+    print "ERROR : No code related characters in sumb.pyf cannot contain upper case characters"
+    print "ERROR : Uppercase found in sumb.pyf line {0:d}. String found is: {1:s} ".format(i,line)    
+    sys.exit(1)
 
+
+# Define charachter set
 ignoreChars = set("!#")
 
+f = open("sumb.pyf","r")
 for i, line in enumerate(f):
     line = line.rstrip()
     if not line:
         # Line is empty
         continue
+    elif line.islower():
+        # Check if the line contains all lower case. 
+        continue
     elif any((c in ignoreChars) for c in line):
         # Check for comments and preprocessor tags
+        
+        # Comments are special since they might be trailing actual code, need to check further
+        if "!" in line:
+            tmp = line.split("!")[0]  # Get the text left of comment
+            tmp = tmp.replace(" ","") # Clean all white spaces since they will show as not lower case later
+            
+            if not tmp:
+                # Check if the rest is an empty string after the manipulation
+                continue
+            
+            if not tmp.islower():
+                # Check if the rest contains upper case characters
+                error(i,line)
+        # No upper case characters was found
         continue
-    elif line.islower():
-        # Check if the line contains all lower case. If not we adjust
-        continue
+        
     else:
-         print "ERROR : Uppercase found in sumb.pyf line {0:d}. String found is: {1:s} ".format(i,line)
-         print "ERROR : No code related characters in sumb.pyf cannot contain upper case characters"
-         sys.exit(1)
+         error(i,line)
 
 # No errors in file exit with no errors
 sys.exit(0)
         
+
 
