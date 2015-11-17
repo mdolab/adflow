f895a63a0e2bca097d9675578d41ede6ebce7a2c
kenway <kenway@localhost>
2015-11-17 11:40:15 -0500

    Minor indentation fix for convergenceInfo that killed the complex build. No code changed here.

--- src/solver/convergenceInfo.F90
+++ src/solver/convergenceInfo.F90
@@ -379,7 +379,7 @@ subroutine convergenceInfo
               if (totalR < L2ConvThisLevel * totalR0) then 
                  absConv = .True. 
               end if
-              
+
               ! Relative check only done on finest level
               if (currentLevel == 1) then 
                  if (totalR < L2ConvThisLevelRel*totalRStart) then 
@@ -394,192 +394,192 @@ subroutine convergenceInfo
               ! If we are not higher than the min iteration num, alwasy "not converged"
               converged = .False.
            end if
-           
-        !===========================================================
 
-     case (unsteady)
+           !===========================================================
 
-        ! Unsteady mode. The array convArray may not be present
-        ! and therefore something else must be done.
-        ! First determine the position in the array timeDataArray
-        ! that can be used. For the coarser grids this is 1,
-        ! because the time evolution is overwritten. For the fine
-        ! mesh the actual position is determined.
+        case (unsteady)
 
-        nn = 1
-        if(groundLevel == 1) &
-             nn = timeStepUnsteady + nTimeStepsRestart
-        nn = max(nn,1_intType)
+           ! Unsteady mode. The array convArray may not be present
+           ! and therefore something else must be done.
+           ! First determine the position in the array timeDataArray
+           ! that can be used. For the coarser grids this is 1,
+           ! because the time evolution is overwritten. For the fine
+           ! mesh the actual position is determined.
 
-        ! Make a distinction between the time integration
-        ! schemes.
+           nn = 1
+           if(groundLevel == 1) &
+                nn = timeStepUnsteady + nTimeStepsRestart
+           nn = max(nn,1_intType)
 
-
-        select case(timeIntegrationScheme)
-
-        case (explicitRK)
-
-           ! Explicit scheme. Simply store the data in the
-           ! convergence arrays.
-
-           timeArray(nn) = timeUnsteady + timeUnsteadyRestart
-
-           ! For explicit schemes the residuals are not
-           ! monitored and therefore the monitoring variables
-           ! can simply be copied.
-
-           do mm=1,nMon
-              timeDataArray(nn,mm) = monGlob(mm)
-           enddo
-
-           !=======================================================
-
-        case (MD,BDF,implicitRK)
-
-           ! An implicit scheme is used and therefore an
-           ! iterative algorithm within every time step.
-           ! The array convArray may not be present and
-           ! therefore
-           ! something else must be done. First determine the
-           ! position in the array timeDataArray that can be
-           ! used.
-           ! For the coarser grids this is 1, because the time
-           ! evolution is overwritten. For the fine mesh the
-           ! actual position is determined.
-
-           ! Determine the situation we have here.
-
-           testInitUnsteady: if(iterTot == 0) then
-
-              ! This is the initialization phase for this time step.
-              ! Simply copy monGlob into monRef, store the value
-              ! of the physical time and set converged to .false..
+           ! Make a distinction between the time integration
+           ! schemes.
 
 
-              do mm=1,nMon
-                 monRef(mm) = monGlob(mm)
-              enddo
+           select case(timeIntegrationScheme)
+
+           case (explicitRK)
+
+              ! Explicit scheme. Simply store the data in the
+              ! convergence arrays.
 
               timeArray(nn) = timeUnsteady + timeUnsteadyRestart
-              converged     = .false.
 
-           else testInitUnsteady
-
-              ! Iteration for this time step. Store the relative
-              ! convergence for the residual compared to the start
-              ! of this time step; an absolute norm does not give
-              ! any information here. For all other monitoring 
-              ! variables the current value is stored.
+              ! For explicit schemes the residuals are not
+              ! monitored and therefore the monitoring variables
+              ! can simply be copied.
 
               do mm=1,nMon
-                 select case (monNames(mm))
-
-                 case (cgnsL2resRho,  cgnsL2resMomx,    &
-                      cgnsL2resMomy, cgnsL2resMomz,    &
-                      cgnsL2resRhoE, cgnsL2resNu,      &
-                      cgnsL2resK,    cgnsL2resOmega,   &
-                      cgnsL2resTau,  cgnsL2resEpsilon, &
-                      cgnsL2resV2,   cgnsL2resF)
-
-                    timeDataArray(nn,mm) = monGlob(mm) &
-                         / max(monRef(mm),eps)
-
-                    !===============================================
-
-                 case default
-
-
-                    timeDataArray(nn,mm) = monGlob(mm)
-
-                 end select
+                 timeDataArray(nn,mm) = monGlob(mm)
               enddo
 
-              ! Set the logical converged to .false. if the density
-              ! residual has not converged yet.
+              !=======================================================
 
-              if(timeDataArray(nn,1) < L2ConvThisLevel) &
-                   converged = .True.
+           case (MD,BDF,implicitRK)
 
-           endif testInitUnsteady
-        end select ! temporal integration scheme
+              ! An implicit scheme is used and therefore an
+              ! iterative algorithm within every time step.
+              ! The array convArray may not be present and
+              ! therefore
+              ! something else must be done. First determine the
+              ! position in the array timeDataArray that can be
+              ! used.
+              ! For the coarser grids this is 1, because the time
+              ! evolution is overwritten. For the fine mesh the
+              ! actual position is determined.
 
-     end select ! unsteady
+              ! Determine the situation we have here.
 
-  endif testRootProc
-enddo spectralLoop
+              testInitUnsteady: if(iterTot == 0) then
 
-! Check if a NaN occured. If so the computation is returnFaild,
-! such that possible solution files written earlier are not
-! corrupted.
+                 ! This is the initialization phase for this time step.
+                 ! Simply copy monGlob into monRef, store the value
+                 ! of the physical time and set converged to .false..
 
-call mpi_bcast(nanOccurred, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
 
-if( nanOccurred )then
+                 do mm=1,nMon
+                    monRef(mm) = monGlob(mm)
+                 enddo
 
-  if (myid == 0) then 
-     print *,'Nan occured in Convergence Info on proc:',myid
+                 timeArray(nn) = timeUnsteady + timeUnsteadyRestart
+                 converged     = .false.
+
+              else testInitUnsteady
+
+                 ! Iteration for this time step. Store the relative
+                 ! convergence for the residual compared to the start
+                 ! of this time step; an absolute norm does not give
+                 ! any information here. For all other monitoring 
+                 ! variables the current value is stored.
+
+                 do mm=1,nMon
+                    select case (monNames(mm))
+
+                    case (cgnsL2resRho,  cgnsL2resMomx,    &
+                         cgnsL2resMomy, cgnsL2resMomz,    &
+                         cgnsL2resRhoE, cgnsL2resNu,      &
+                         cgnsL2resK,    cgnsL2resOmega,   &
+                         cgnsL2resTau,  cgnsL2resEpsilon, &
+                         cgnsL2resV2,   cgnsL2resF)
+
+                       timeDataArray(nn,mm) = monGlob(mm) &
+                            / max(monRef(mm),eps)
+
+                       !===============================================
+
+                    case default
+
+
+                       timeDataArray(nn,mm) = monGlob(mm)
+
+                    end select
+                 enddo
+
+                 ! Set the logical converged to .false. if the density
+                 ! residual has not converged yet.
+
+                 if(timeDataArray(nn,1) < L2ConvThisLevel) &
+                      converged = .True.
+
+              endif testInitUnsteady
+           end select ! temporal integration scheme
+
+        end select ! unsteady
+
+     endif testRootProc
+  enddo spectralLoop
+
+  ! Check if a NaN occured. If so the computation is returnFaild,
+  ! such that possible solution files written earlier are not
+  ! corrupted.
+
+  call mpi_bcast(nanOccurred, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
+
+  if( nanOccurred )then
+
+     if (myid == 0) then 
+        print *,'Nan occured in Convergence Info on proc:',myid
+     end if
+
+     routineFailed = .True.
+
+     ! Initialize the flow field to uniform flow.
+     tempMGStartLevel = mgStartLevel
+     tempCurrentLevel = currentLevel
+
+     mgStartLevel = 1
+     currentlevel = 1
+
+     call setUniformFlow
+
+     mgStartLevel = tempMGStartLevel
+     currentLevel = tempCurrentLevel
+
+     call returnFail("convergenceInfo", &
+          "A NaN occurred during the computation.")
+
+     ! in a normal computation, code will simply exit.
+     ! in a python based computation, code will set 
+     ! routinedFailed to .True. and return to the 
+     ! python level...
+     return
+  endif
+
+  ! If we are at the max iteration limit but the residual is
+  ! *close*, ie within maxL2DeviationFactor we say that's fine
+
+  if(fromPython .and. groundLevel ==1 .and. nIterCur >=nCycles) then
+
+     !Check to see if residuals are diverging or stalled for python
+     select case (equationMode)
+
+     case (steady, timeSpectral)
+
+        ! Steady or time spectral mode. The convergence histories
+        ! are stored and this info can be used. If the residuals 
+        ! are diverging the, logical routineFailed in killSignals
+        ! is set to true and the progress is halted.
+        !only check on root porcessor
+        if (myID == 0) then
+
+           ! If we made it to ncycles, check to see if we're
+           ! "close" to being converged. 
+           do sps = 1,nTimeIntervalsSpectral
+              if (totalR < maxL2DeviationFactor * totalR0 * L2ConvThisLevel) then 
+                 routineFailed = .True.
+              end if
+           enddo
+        endif
+
+        call mpi_bcast(routineFailed, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
+
+     end select
   end if
 
-  routineFailed = .True.
+  ! Determine whether or not the solution is considered
+  ! converged.  This info is only known at processor 0 and must
+  ! therefore be broadcast to the other processors.
 
-  ! Initialize the flow field to uniform flow.
-  tempMGStartLevel = mgStartLevel
-  tempCurrentLevel = currentLevel
-
-  mgStartLevel = 1
-  currentlevel = 1
-
-  call setUniformFlow
-
-  mgStartLevel = tempMGStartLevel
-  currentLevel = tempCurrentLevel
-
-  call returnFail("convergenceInfo", &
-       "A NaN occurred during the computation.")
-
-  ! in a normal computation, code will simply exit.
-  ! in a python based computation, code will set 
-  ! routinedFailed to .True. and return to the 
-  ! python level...
-  return
-endif
-
-! If we are at the max iteration limit but the residual is
-! *close*, ie within maxL2DeviationFactor we say that's fine
-
-if(fromPython .and. groundLevel ==1 .and. nIterCur >=nCycles) then
-
-   !Check to see if residuals are diverging or stalled for python
-  select case (equationMode)
-
-  case (steady, timeSpectral)
-
-     ! Steady or time spectral mode. The convergence histories
-     ! are stored and this info can be used. If the residuals 
-     ! are diverging the, logical routineFailed in killSignals
-     ! is set to true and the progress is halted.
-     !only check on root porcessor
-     if (myID == 0) then
-
-        ! If we made it to ncycles, check to see if we're
-        ! "close" to being converged. 
-        do sps = 1,nTimeIntervalsSpectral
-           if (totalR < maxL2DeviationFactor * totalR0 * L2ConvThisLevel) then 
-              routineFailed = .True.
-           end if
-        enddo
-     endif
-
-     call mpi_bcast(routineFailed, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
-
-  end select
-end if
-
-! Determine whether or not the solution is considered
-! converged.  This info is only known at processor 0 and must
-! therefore be broadcast to the other processors.
-
-call mpi_bcast(converged, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
+  call mpi_bcast(converged, 1, MPI_LOGICAL, 0, SUmb_comm_world, ierr)
 
 end subroutine convergenceInfo
 
@@ -596,34 +596,34 @@ subroutine sumResiduals(nn, mm)
   !      *                                                                *
   !      ******************************************************************
   !
-use blockPointers
-use monitor
-implicit none
-!
-!      Subroutine arguments.
-!
-integer(kind=intType), intent(in) :: nn, mm
-!
-!      Local variables.
-!
-integer(kind=intType) :: i, j, k
-!
-!      ******************************************************************
-!      *                                                                *
-!      * Begin execution                                                *
-!      *                                                                *
-!      ******************************************************************
-!
-! Loop over the number of owned cells of this block and
-! accumulate the residual.
+  use blockPointers
+  use monitor
+  implicit none
+  !
+  !      Subroutine arguments.
+  !
+  integer(kind=intType), intent(in) :: nn, mm
+  !
+  !      Local variables.
+  !
+  integer(kind=intType) :: i, j, k
+  !
+  !      ******************************************************************
+  !      *                                                                *
+  !      * Begin execution                                                *
+  !      *                                                                *
+  !      ******************************************************************
+  !
+  ! Loop over the number of owned cells of this block and
+  ! accumulate the residual.
 
-do k=2,kl
-  do j=2,jl
-     do i=2,il
-        monLoc(mm) = monLoc(mm) + (dw(i,j,k,nn)/vol(i,j,k))**2
+  do k=2,kl
+     do j=2,jl
+        do i=2,il
+           monLoc(mm) = monLoc(mm) + (dw(i,j,k,nn)/vol(i,j,k))**2
+        enddo
      enddo
   enddo
-enddo
 
 end subroutine sumResiduals
 
@@ -636,81 +636,81 @@ subroutine sumAllResiduals(mm)
   !      *                                                                *
   !      ******************************************************************
   !
-use blockPointers
-use monitor
-use flowvarrefstate
-use inputIteration
-implicit none
-!
-!      Subroutine arguments.
-!
-integer(kind=intType), intent(in) :: mm
-!
-!      Local variables.
-!
-integer(kind=intType) :: i, j, k, l
-real(kind=realType) :: state_sum,ovv
+  use blockPointers
+  use monitor
+  use flowvarrefstate
+  use inputIteration
+  implicit none
+  !
+  !      Subroutine arguments.
+  !
+  integer(kind=intType), intent(in) :: mm
+  !
+  !      Local variables.
+  !
+  integer(kind=intType) :: i, j, k, l
+  real(kind=realType) :: state_sum,ovv
 
-!      ******************************************************************
-!      *                                                                *
-!      * Begin execution                                                *
-!      *                                                                *
-!      ******************************************************************
-!
-! Loop over the number of owned cells of this block and
-! accumulate the residual.
+  !      ******************************************************************
+  !      *                                                                *
+  !      * Begin execution                                                *
+  !      *                                                                *
+  !      ******************************************************************
+  !
+  ! Loop over the number of owned cells of this block and
+  ! accumulate the residual.
 
-do k=2,kl
-  do j=2,jl
-     do i=2,il
-        state_sum = 0.0
-        ovv = one/vol(i,j,k)
-        do l=1,nwf
-           state_sum = state_sum + (dw(i,j,k,l)*ovv)**2
-        end do
-        do l=nt1,nt2
-           ! l-nt1+1 will index the turbResScale properly
-           state_sum = state_sum + (dw(i,j,k,l)*ovv*turbResScale(l-nt1+1))**2
-        end do
-        monLoc(mm) = monLoc(mm) + state_sum
+  do k=2,kl
+     do j=2,jl
+        do i=2,il
+           state_sum = 0.0
+           ovv = one/vol(i,j,k)
+           do l=1,nwf
+              state_sum = state_sum + (dw(i,j,k,l)*ovv)**2
+           end do
+           do l=nt1,nt2
+              ! l-nt1+1 will index the turbResScale properly
+              state_sum = state_sum + (dw(i,j,k,l)*ovv*turbResScale(l-nt1+1))**2
+           end do
+           monLoc(mm) = monLoc(mm) + state_sum
+        enddo
      enddo
   enddo
-enddo
 
 end subroutine sumAllResiduals
 
 subroutine printCurrentR()
-use communication 
-use block
-use blockPointers
-use flowvarrefstate
-use inputtimespectral
-implicit none
+  use communication 
+  use block
+  use blockPointers
+  use flowvarrefstate
+  use inputtimespectral
+  implicit none
 
-integer(kind=intType) :: nn,sps,i,j,k,l,ierr
-real(kind=realType) :: state_sum,rnorm,ovv
+  integer(kind=intType) :: nn,sps,i,j,k,l,ierr
+  real(kind=realType) :: state_sum,rnorm,ovv
 
-state_sum = 0.0
-spectralLoop: do sps=1,nTimeIntervalsSpectral
-  domains: do nn=1,nDom
-     call setPointers(nn,1,sps)
-     do k=2,kl
-        do j=2,jl
-           do i=2,il
-              ovv = 1/vol(i,j,k)
-              do l=1,nw
-                 state_sum = state_sum + (dw(i,j,k,l)*ovv)**2
+  state_sum = 0.0
+  spectralLoop: do sps=1,nTimeIntervalsSpectral
+     domains: do nn=1,nDom
+        call setPointers(nn,1,sps)
+        do k=2,kl
+           do j=2,jl
+              do i=2,il
+                 ovv = 1/vol(i,j,k)
+                 do l=1,nw
+                    state_sum = state_sum + (dw(i,j,k,l)*ovv)**2
+                 end do
               end do
            end do
         end do
-     end do
-  end do domains
-end do spectralLoop
-call mpi_reduce(state_sum,rnorm,1,sumb_real,mpi_sum,0,&
-    SUmb_comm_world, ierr)
+     end do domains
+  end do spectralLoop
+  call mpi_reduce(state_sum,rnorm,1,sumb_real,mpi_sum,0,&
+       SUmb_comm_world, ierr)
 
-if (myid==0) then
-  print *,'Current R:',sqrt(rnorm)
-end if
+  if (myid==0) then
+     print *,'Current R:',sqrt(rnorm)
+  end if
 
 end subroutine printCurrentR
