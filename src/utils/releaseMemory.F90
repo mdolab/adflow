6141bec74f88e6c09e52ce742a9096a93ba615a9
Gaetan Kenway <gaetank@gmail.com>
2016-04-12 13:28:40 -0400

    Cleanup from last commit with dadi stuff. Added a generic way of doing real and integer communication for 1to1 connections. This will enable the elimination of much code duplication of the halo exchange routine. Firstly, exchangeCellNumbering and exchangeCoorNumbering are eliminated since they can be generically done with the integer exchange. As a bonus, the halo exhange exchange reduces solution runtime by 2% overlall. Since this comes from communication only, it follows it might help with slightly better strong scaling.

--- src/utils/releaseMemory.F90
+++ src/utils/releaseMemory.F90
@@ -881,10 +881,6 @@ subroutine deallocateBlock(nn, level, sps)
        deallocate(flowDoms(nn,level,sps)%w, stat=ierr)
   if(ierr /= 0) deallocationFailure = .true.
 
-  if( associated(flowDoms(nn,level,sps)%dadidata) ) &
-       deallocate(flowDoms(nn,level,sps)%dadidata, stat=ierr)
-  if(ierr /= 0) deallocationFailure = .true.
-
   if( associated(flowDoms(nn,level,sps)%wOld) ) &
        deallocate(flowDoms(nn,level,sps)%wOld, stat=ierr)
   if(ierr /= 0) deallocationFailure = .true.
