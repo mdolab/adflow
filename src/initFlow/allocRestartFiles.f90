bbac257825f3ae8a8e8a9b7dea8a3642781c4890
Eirikur Jonsson <eirikurj@umich.edu>
2016-06-21 08:43:07 -0400

    Fix reg test. Adding missing file from previous commit

---
+++ src/initFlow/allocRestartFiles.f90
@@ -0,0 +1,50 @@
+!
+!      ******************************************************************
+!      *                                                                *
+!      * File:          allocRestartFiles.f90                           *
+!      * Author:        Eirikur Jonsson                                 *
+!      *                                                                *
+!      ******************************************************************
+!
+subroutine allocRestartFiles(nFiles)
+  !
+  !      ******************************************************************
+  !      *    Allocate memory for the restartfles                         *                                   *
+  !      *    The array is populated from Python using setRestartFiles    *
+  !      *    If memeory has been allocated for the array there exist at  *
+  !      *    least one element in the array.                             *
+  !      ******************************************************************
+  !
+  use constants
+  use inputIO
+  use inputTimeSpectral
+  implicit none
+  !
+  !      Subroutine argument.
+  !
+  integer(kind=intType) :: nFiles
+  !
+  !      Local variables.
+  !
+  integer :: ierr
+  !      ******************************************************************
+  !      *                                                                *
+  !      * Begin execution                                                *
+  !      *                                                                *
+  !      ******************************************************************
+  !
+
+  if (allocated(restartFiles)) then
+     deallocate(restartFiles)
+  end if
+
+  allocate(restartFiles(nFiles), stat=ierr)
+  if(ierr /= 0)                          &
+    call returnFail("allocRestartFiles", &
+            "Memory allocation failure for restartFiles")
+  
+  ! Zero Array with empty strings
+  restartFiles = ""
+
+end subroutine allocRestartFiles
+
