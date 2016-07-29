f93067ee373393bf71c6babe0d23b3fd63826178
Gaetan Kenway <gaetank@gmail.com>
2016-07-29 15:42:04 -0400

    Refactoring of zipper mesh computation. UNTESTED.

--- src/overset/createZipperMesh.F90
+++ src/overset/createZipperMesh.F90
@@ -25,8 +25,11 @@ subroutine createZipperMesh(level, sps, oWallSendList, oWallRecvList, &
   use overset
   use BCTypes
   use inputTimeSpectral
+  use wallDistanceData, only : xVolumeVec, IS1
+  use stringops
   use inputOverset
-
+  use adtapi
+  use adjointvars
   implicit none
 
   ! Input Parameters
@@ -38,18 +41,29 @@ subroutine createZipperMesh(level, sps, oWallSendList, oWallRecvList, &
 
   ! Local Variables
   integer(kind=intType) :: i, j, k, ii, jj, kk, iStart, iSize
-  integer(kind=intType) :: iDom, jDom, nn, mm, n, ierr, iProc, iWork
+  integer(kind=intType) :: iDom, jDom, nn, mm, n, ierr, iProc, iWork, nStrings
   integer(kind=intType), dimension(:,:), allocatable :: tmpInt2D
   logical, dimension(:), allocatable :: oWallReady
   type(oversetWall), dimension(:), allocatable :: oWalls
   integer(kind=intType), dimension(:), allocatable :: intRecvBuf
   logical :: isWallType
+  type(oversetString) :: master, pocketMaster
+  integer(kind=intType), dimension(:), allocatable :: nodeIndices, cellIndices
+  integer(kind=intType), dimension(:), allocatable :: nElemsProc, nNodesProc
+
+  type(oversetString), dimension(:), allocatable, target :: strings
+  integer(kind=intType) :: nFullStrings, nUnique
 
   ! MPI/Communication related
   integer status(MPI_STATUS_SIZE) 
   integer(kind=intType), dimension(:, :), allocatable :: bufSizes
   integer(kind=intType), dimension(:, :), allocatable :: recvInfo
   integer(kind=intType) :: sendCount, recvCount, index
+
+  ! Wall search related
+  integer(kind=intType) :: ncells
+  type(oversetWall), dimension(:), allocatable, target :: walls
+  type(oversetWall),  target :: fullWall
 
   ! -------------------------------------------------------------------
   ! Step 1: Eliminate any gap overlaps between meshes
@@ -266,7 +280,136 @@ subroutine createZipperMesh(level, sps, oWallSendList, oWallRecvList, &
      call writeWalls(oWalls, size(oWalls))
   end if
 
-  call makeGapBoundaryStrings(level, sps)
+  call makeGapBoundaryStrings(level, sps, master)
   
+  if (myid == 0) then 
+     
+     if (debugZipper) then 
+        call writeZipperDebug(master)
+     end if
+
+     call createOrderedStrings(master, strings, nStrings)
+     call performSelfZip(master, strings, nStrings)
+  
+     ! Write out any self-zipped triangles
+     if (debugZipper) then 
+        call writeOversetTriangles(master, "selfzipTriangulation.dat")
+     end if
+
+     call makeCrossZip(master, strings, nStrings)
+
+     if (debugZipper) then 
+        call writeOversetTriangles(master, "fullTriangulation.dat")
+     end if
+
+     ! ---------------------------------------------------------------
+     ! Sort through zipped triangle edges and the edges which have not
+     ! been used twice (orphan edges) will be ultimately gathered to 
+     ! form polygon pockets to be zipped.
+     call makePocketZip(master, strings, nStrings, pocketMaster)
+
+     if (debugZipper) then 
+        call writeOversetTriangles(pocketMaster, "pocketTriangulation.dat")
+     end if
+
+     ! -------------------------------------------------------------
+     ! Perform comm data preparation for force integration on zipper
+     ! triangles. Do containment search for zipper triangle
+     ! containement in primal wall quad cells. Here 'surfCellID'
+     ! store the indices of the global cells containing zipper
+     ! triangle's cell center.
+     ! (Look at ../wallDistance/determineWallAssociation.F90).
+
+     call determineZipperWallAssociation(master, pocketMaster, fullWall)
+
+    
+     ! (3 nodes per triangle and 3 DOF per ndoe)
+     allocate(nodeIndices(3*3*(master%ntris + pocketMaster%ntris)))
+  
+     ii = 0
+     do i=1, master%nTris
+        do j=1, 3 ! Triangle node loop
+           jj = master%tris(j, i)
+           do k=1, 3 ! DOF Loop 
+              ! Zero-based ordering for petsc
+              nodeIndices(9*ii+3*(j-1)+k) = 3*master%ind(jj) + k-1 
+           end do
+        end do
+        ii = ii + 1
+     end do
+     
+     do i=1, pocketMaster%nTris
+        do j=1, 3 ! Triangle node loop
+           jj = pocketMaster%tris(j, i)
+           do k=1, 3 ! DOF Loop 
+              ! Zero-based ordering for petsc
+              nodeIndices(9*ii+3*(j-1)+k) = 3*pocketMaster%ind(jj) + k-1 
+           end do
+        end do
+        ii = ii + 1
+     end do
+  else
+     ! Other procs don't have any triangles :-(
+     allocate(nodeIndices(0))
+     allocate(cellIndices(0))
+  end if
+  
+  ! Do not need walls, fullWall or the strings anymore. Deallocate them.
+  do i=1, nClusters
+     deallocate(walls(i)%x, walls(i)%conn, walls(i)%ind)
+     call destroySerialQuad(walls(i)%ADT)
+  end do
+  deallocate(walls)
+
+  deallocate(fullWall%x, fullWall%conn, fullWall%ind)
+  call destroySerialQuad(fullWall%ADT)
+
+  ! Clean up memory on the root proc
+  if (myid == 0) then 
+     do i=1, nFullStrings
+        call deallocateString(strings(i))
+     end do
+     deallocate(strings)
+     call deallocateString(master)
+     call deallocateString(pocketMaster)
+  end if
+
+  ! ------------------------------------------------
+
+  ! These three vectors are the MPI vectors with only non-zero size on
+  ! the root proc. This is where we scatter the nodes, pressure
+  ! tractions and viscous tractions into. 
+  call VecCreateMPI(SUMB_COMM_WORLD, size(nodeIndices), PETSC_DETERMINE, &
+       localZipperNodes, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  call VecDuplicate(localZipperNodes, localZipperTp, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  call VecDuplicate(localZipperNodes, localZipperTv, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  ! This is a generic global nodal vector that can store the nodal
+  ! values, pressure tractions or viscous tractions. 
+  call VecCreateMPI(SUMB_COMM_WORLD, 3*nNodesLocal(1), PETSC_DETERMINE, &
+       globalNodalVec, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  ! Now create the general scatter that goes from the
+  ! globalNodalVector to the local vectors. 
+  call ISCreateGeneral(sumb_comm_world, size(nodeIndices), &
+       nodeIndices, PETSC_COPY_VALUES, IS1, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  call VecScatterCreate(globalNodalVec, IS1, localZipperNodes, PETSC_NULL_OBJECT, &
+       nodeZipperScatter, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+
+  call ISDestroy(IS1, ierr)
+  call EChk(ierr,__FILE__,__LINE__)
+ 
+  ! Free the remaining memory
+  deallocate(nElemsProc, nNodesProc, nodeIndices, cellIndices)
+
 end subroutine createZipperMesh
 
