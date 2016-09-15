ff7605e24390489132d62b7141e671549903fcbb
kenway <kenway@localhost>
2016-09-15 10:55:13 -0400

    Fixed a relatively major issue with regressions. The forward mode AD code was not updating the coordinates after xhalo, so the coordinates were permenantly modified. This means while the all the AD was correct, it was linearizing about a different solution. For this reason all the tests that had used forward mode AD, (1-8) have been updated to reflect this.

--- src/adjoint/outputReverse/bcroutines_b.f90
+++ src/adjoint/outputReverse/bcroutines_b.f90
@@ -1784,9 +1784,10 @@ contains
     use constants
     use blockpointers, only : bcdata, bcdatad
     use inputdiscretization, only : viscwallbctreatment
-    use bcpointers_b, only : ww1, ww1d, ww2, ww2d, rlv1, rlv1d, rlv2, &
-&   rlv2d, pp0, pp0d, pp1, pp1d, pp2, pp2d, pp3, pp3d, rev1, rev1d, rev2&
-&   , rev2d, istart, jstart, isize, jsize
+    use bcpointers_b, only : ww0, ww0d, ww1, ww1d, ww2, ww2d, rlv0, &
+&   rlv0d, rlv1, rlv1d, rlv2, rlv2d, pp0, pp0d, pp1, pp1d, pp2, pp2d, &
+&   pp3, pp3d, rev0, rev0d, rev1, rev1d, rev2, rev2d, istart, jstart, &
+&   isize, jsize
     use flowvarrefstate, only : viscous, eddymodel
     use iteration, only : currentlevel, groundlevel
     implicit none
@@ -1924,8 +1925,8 @@ contains
     use constants
     use blockpointers, only : bcdata
     use inputdiscretization, only : viscwallbctreatment
-    use bcpointers_b, only : ww1, ww2, rlv1, rlv2, pp0, pp1, pp2, pp3, &
-&   rev1, rev2, istart, jstart, isize, jsize
+    use bcpointers_b, only : ww0, ww1, ww2, rlv0, rlv1, rlv2, pp0, pp1, &
+&   pp2, pp3, rev0, rev1, rev2, istart, jstart, isize, jsize
     use flowvarrefstate, only : viscous, eddymodel
     use iteration, only : currentlevel, groundlevel
     implicit none
@@ -1997,9 +1998,10 @@ contains
     use constants
     use blockpointers, only : bcdata, bcdatad
     use inputdiscretization, only : viscwallbctreatment
-    use bcpointers_b, only : ww1, ww1d, ww2, ww2d, rlv1, rlv1d, rlv2, &
-&   rlv2d, pp1, pp1d, pp2, pp2d, pp3, pp3d, rev1, rev1d, rev2, rev2d, &
-&   istart, jstart, isize, jsize
+    use bcpointers_b, only : ww0, ww0d, ww1, ww1d, ww2, ww2d, rlv0, &
+&   rlv0d, rlv1, rlv1d, rlv2, rlv2d, pp0, pp0d, pp1, pp1d, pp2, pp2d, &
+&   pp3, pp3d, rev0, rev0d, rev1, rev1d, rev2, rev2d, istart, jstart, &
+&   isize, jsize
     use flowvarrefstate, only : viscous, eddymodel, rgas, rgasd
     use iteration, only : currentlevel, groundlevel
     implicit none
@@ -2213,8 +2215,8 @@ contains
     use constants
     use blockpointers, only : bcdata
     use inputdiscretization, only : viscwallbctreatment
-    use bcpointers_b, only : ww1, ww2, rlv1, rlv2, pp1, pp2, pp3, rev1, &
-&   rev2, istart, jstart, isize, jsize
+    use bcpointers_b, only : ww0, ww1, ww2, rlv0, rlv1, rlv2, pp0, pp1, &
+&   pp2, pp3, rev0, rev1, rev2, istart, jstart, isize, jsize
     use flowvarrefstate, only : viscous, eddymodel, rgas
     use iteration, only : currentlevel, groundlevel
     implicit none
@@ -3352,9 +3354,10 @@ contains
     use inputdiscretization, only : eulerwallbctreatment
     use iteration, only : currentlevel, groundlevel
     use utils_b, only : mydim, mydim_b
-    use bcpointers_b, only : ww1, ww1d, pp1, pp1d, rlv1, rlv1d, rev1, &
-&   rev1d, ww2, ww2d, pp2, pp2d, rlv2, rlv2d, rev2, rev2d, pp3, pp3d, ss&
-&   , ssi, ssid, ssj, ssk, istart, isize, jstart, jsize, iend, jend
+    use bcpointers_b, only : ww0, ww0d, ww1, ww1d, ww2, ww2d, pp0, pp0d,&
+&   pp1, pp1d, pp2, pp2d, pp3, pp3d, rlv0, rlv0d, rlv1, rlv1d, rlv2, &
+&   rlv2d, rev0, rev0d, rev1, rev1d, rev2, rev2d, ss, ssi, ssid, ssj, &
+&   ssk, istart, isize, jstart, jsize, iend, jend
     implicit none
 ! subroutine arguments.
     logical, intent(in) :: secondhalo, correctfork
@@ -3507,8 +3510,9 @@ contains
     use inputdiscretization, only : eulerwallbctreatment
     use iteration, only : currentlevel, groundlevel
     use utils_b, only : mydim
-    use bcpointers_b, only : ww1, pp1, rlv1, rev1, ww2, pp2, rlv2, rev2,&
-&   pp3, ss, ssi, ssj, ssk, istart, isize, jstart, jsize, iend, jend
+    use bcpointers_b, only : ww0, ww1, ww2, pp0, pp1, pp2, pp3, rlv0, &
+&   rlv1, rlv2, rev0, rev1, rev2, ss, ssi, ssj, ssk, istart, isize, &
+&   jstart, jsize, iend, jend
     implicit none
 ! subroutine arguments.
     logical, intent(in) :: secondhalo, correctfork
