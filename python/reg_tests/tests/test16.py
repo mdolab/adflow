835b00bfd51fe11ab47f50be3ed90629e72907bc
Justin Gray <justin.s.gray@gmail.com>
2017-02-08 15:07:41 -0500

    half way done with edot

--- python/reg_tests/tests/test16.py
+++ python/reg_tests/tests/test16.py
@@ -50,7 +50,7 @@ options = {
     'flowtype':'internal'
 }
 
-ap = AeroProblem(name='conv_nozzle', alpha=0.0,  mach=0.25, T=500, P=79326.7,
+ap = AeroProblem(name='conv_nozzle', alpha=30.0,  mach=0.25, T=500, P=79326.7,
                  areaRef=1., chordRef=2.,
                  evalFuncs=['mdot', 'mdot_up', 'mdot_down',
                             'mavgptot_up', 'mavgptot_down',
@@ -60,7 +60,7 @@ ap = AeroProblem(name='conv_nozzle', alpha=0.0,  mach=0.25, T=500, P=79326.7,
                             'thrust', 
                             'pk_up', 'pk_down', 
                             'distortionmn', 'distortionptot'
-                            ])
+                            ], )
 
 # Creat the solver
 
