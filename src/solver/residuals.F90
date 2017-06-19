243fbfc6a9035d2013f6b63bca77b535edd5f817
Anil Yildirim <anily@umich.edu>
2017-06-19 14:24:12 -0400

    Moved the assignment of pointers for the Shock Sensor to setPointers subroutine

--- src/solver/residuals.F90
+++ src/solver/residuals.F90
@@ -975,7 +975,6 @@ contains
     !
     use blockPointers
     use constants
-    use inputDiscretization, only : lumpedDiss
     use inputTimeSpectral
     use Iteration
     use utils, only : setPointers
@@ -996,11 +995,6 @@ contains
           ! Set the pointers for this block.
 
           call setPointers(nn, currentLevel, sps)
-          
-          ! Check if we are using approximate fluxes, if so, the correct pointers to shock sensor must be set
-          if (lumpedDiss) then
-            shockSensor => flowDoms(nn,1,sps)%shockSensor
-          end if 
           
           call residual_block
 
