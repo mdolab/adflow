b2e6b9294be1b0eb2c9fe3c749529cb5ea60e083
Ney Secco <neysecco@users.noreply.github.com>
2017-07-31 13:44:45 -0400

    Forgot to fix QCR in other directions.

--- src/solver/fluxes.F90
+++ src/solver/fluxes.F90
@@ -3015,7 +3015,7 @@ contains
                    ! Compute off-diagonal terms of vorticity tensor (we will ommit the 1/2)
                    Wxy = u_y - v_x
                    Wxz = u_z - w_x
-                   Wyz = u_y - v_x
+                   Wyz = v_z - w_y
                    Wyx = -Wxy
                    Wzx = -Wxz
                    Wzy = -Wyz
@@ -3291,7 +3291,7 @@ contains
                    ! Compute off-diagonal terms of vorticity tensor (we will ommit the 1/2)
                    Wxy = u_y - v_x
                    Wxz = u_z - w_x
-                   Wyz = u_y - v_x
+                   Wyz = v_z - w_y
                    Wyx = -Wxy
                    Wzx = -Wxz
                    Wzy = -Wyz
