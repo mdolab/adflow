590a3191510b1d8bf64bc756327d83bc717c1cc1
Gaetan Kenway <gaetank@gmail.com>
2017-10-16 16:56:11 -0700

    added the ability to use CGNS 3.3 module-based library. The old 3.2.x library still works and is the default in the example config files.

--- config/defaults/config.LINUX_INTEL.mk
+++ config/defaults/config.LINUX_INTEL.mk
@@ -1,5 +1,5 @@
 # ----------------------------------------------------------------------
-# Config file for Intel ifort 
+# Config file for Intel ifort
 # ----------------------------------------------------------------------
 
 # ------- Define a possible parallel make (use PMAKE = make otherwise)--
@@ -15,16 +15,29 @@ CC   = mpicc
 # Default (nothing specified) is 4 byte integers and 8 byte reals
 
 FF90_INTEGER_PRECISION_FLAG =
-FF90_REAL_PRECISION_FLAG    = 
+FF90_REAL_PRECISION_FLAG    =
 CC_INTEGER_PRECISION_FLAG   =
-CC_REAL_PRECISION_FLAG      = 
+CC_REAL_PRECISION_FLAG      =
 
 # ------- Define CGNS Inlcude and linker flags -------------------------
+# Define the CNGS include directory and linking flags for CGNSlib. We
+# can use 3.2.x OR CGNS 3.3+. You must define which version is being
+# employed as shown below. We are assuming that HDF5 came from PETSc
+# so it is included in ${PETSC_LIB}. Otherwise you will have to
+# specify the HDF5 library.
+
+# ----------- CGNS 3.2.x ------------------
+CGNS_VERSION_FLAG=
 CGNS_INCLUDE_FLAGS=-I$(HOME)/packages/cgnslib_3.2.1/src
 CGNS_LINKER_FLAGS=-L$(HOME)/packages/cgnslib_3.2.1/src -lcgns
 
+# # ----------- CGNS 3.3.x ------------------
+# CGNS_VERSION_FLAG=-DUSECGNSMODULE
+# CGNS_INCLUDE_FLAGS=-I$(HOME)/packages/CGNS/src
+# CGNS_LINKER_FLAGS=-L$(HOME)/packages/CGNS/src/lib -lcgns
+
 # ------- Define Compiler Flags ----------------------------------------
-FF90_FLAGS   = -DHAS_ISNAN -fPIC -r8 -O2 -g 
+FF90_FLAGS   = -DHAS_ISNAN -fPIC -r8 -O2 -g
 C_FLAGS      = -DHAS_ISNAN -O -fPIC
 
 # ------- Define Archiver  and Flags -----------------------------------
