fe983e1ee3c6f489e9d17c5bf46e0c513e359e79
Justin Gray <justin.s.gray@gmail.com>
2017-11-20 23:42:56 -0500

    added tests 2 and 3

--- python/reg_tests/om_tests/test_std_compare.py
+++ python/reg_tests/om_tests/test_std_compare.py
@@ -25,36 +25,53 @@ from python.om_adflow import OM_ADFLOW
 from om_commonUtils import standardCompare
 
 
-
-
 class StandardCompareTests(unittest.TestCase):
 
-    def setUp(self): 
+
+    def test1(self): 
+        from tests.test1 import options as aero_options, gridFile, ap
+
         solve = True
         if 'solve' not in sys.argv:
             aero_options['restartfile'] = gridFile
             solve = False
-        self.solve = solve
-
-    def test1(self): 
-        from tests.test1 import options as aero_options, gridFile, ap
         
         # Create the solver
         CFDSolver = CFDSolver = ADFLOW(options=aero_options, debug=True)        
         # Create the OM wrapped solver
         OM_adflow = OM_ADFLOW(ap=ap, aero_options=aero_options, debug=True, owns_indeps=True)
         
-        standardCompare(self, CFDSolver, ap, OM_adflow, self.solve)
+        standardCompare(self, CFDSolver, ap, OM_adflow, solve)
 
     def test2(self): 
         from tests.test2 import options as aero_options, gridFile, ap
+
+        solve = True
+        if 'solve' not in sys.argv:
+            aero_options['restartfile'] = gridFile
+            solve = False
         
         # Create the solver
         CFDSolver = CFDSolver = ADFLOW(options=aero_options, debug=True)        
         # Create the OM wrapped solver
         OM_adflow = OM_ADFLOW(ap=ap, aero_options=aero_options, debug=True, owns_indeps=True)
         
-        standardCompare(self, CFDSolver, ap, OM_adflow, self.solve)
+        standardCompare(self, CFDSolver, ap, OM_adflow, solve)
+
+    def test3(self): 
+        from tests.test3 import options as aero_options, gridFile, ap
+
+        solve = True
+        if 'solve' not in sys.argv:
+            aero_options['restartfile'] = gridFile
+            solve = False
+        
+        # Create the solver
+        CFDSolver = CFDSolver = ADFLOW(options=aero_options, debug=True)        
+        # Create the OM wrapped solver
+        OM_adflow = OM_ADFLOW(ap=ap, aero_options=aero_options, debug=True, owns_indeps=True)
+        
+        standardCompare(self, CFDSolver, ap, OM_adflow, solve)
 
 
 if __name__ == "__main__": 
