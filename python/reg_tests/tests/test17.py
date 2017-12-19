2757c73e5595decd56c23da0eb8c77003f627cbe
Gaetan Kenway <gaetank@gmail.com>
2017-12-18 18:10:18 -0800

    fixes for the blocked code to work correctly. test 1-9 now pass

--- python/reg_tests/tests/test17.py
+++ python/reg_tests/tests/test17.py
@@ -7,6 +7,7 @@ from __future__ import print_function
 import sys, os, copy
 from mpi4py import MPI
 from baseclasses import AeroProblem
+sys.path.append(os.path.abspath('./'))
 from mdo_regression_helper import *
 from commonUtils import *
 
@@ -82,10 +83,10 @@ name = 'nozzle'
 # Aerodynamic problem description
 ap = AeroProblem(name=name, alpha=alpha, mach=mach, altitude=altitude,
                  areaRef=areaRef, chordRef=chordRef,
-                 evalFuncs=['mdot_up', 'mdot_down', 'mdot_plane', 
-                            'mavgptot_up', 'mavgptot_down', 'mavgptot_plane', 
+                 evalFuncs=['mdot_up', 'mdot_down', 'mdot_plane',
+                            'mavgptot_up', 'mavgptot_down', 'mavgptot_plane',
                             'mavgttot_up', 'mavgttot_down', 'mavgttot_plane',
-                            'mavgps_up', 'mavgps_down', 'mavgps_plane',     
+                            'mavgps_up', 'mavgps_down', 'mavgps_plane',
                             'sigmamn_up',  'sigmamn_plane',
                             'sigmaptot_up', 'sigmaptot_plane',
                             ])
@@ -100,7 +101,7 @@ ap.addDV('PressureStagnation', family='upstream')
 ap.setBCVar('TemperatureStagnation',  500.0, 'upstream')
 ap.addDV('TemperatureStagnation', family='upstream')
 
- 
+
 CFDSolver = ADFLOW(options=options, debug=True)
 
 CFDSolver.addIntegrationSurface('../inputFiles/integration_plane_viscous.fmt', 'viscous_plane')
@@ -155,5 +156,3 @@ CFDSolver.evalFunctions(ap, funcs)
 if MPI.COMM_WORLD.rank == 0:
     print('Eval Functions:')
     reg_write_dict(funcs, 1e-10, 1e-10)
-
-
