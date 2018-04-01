0183fbd011052bf391b8f44deb2b37ffbbc41d85
Justin Gray <justin.s.gray@gmail.com>
2018-03-31 20:53:46 -0400

    all test working now

--- python/reg_tests/om_tests/test_sol_compare.py
+++ python/reg_tests/om_tests/test_sol_compare.py
@@ -82,9 +82,9 @@ class Tests(unittest.TestCase):
 
 
         assert_rel_error(self, res, prob.model._residuals['states.states'], tolerance=5e-7)    
-        assert_rel_error(self, states, prob['states.states'], tolerance=1e-10)
+        assert_rel_error(self, states, prob['states.states'], tolerance=2e-10)
 
-        assert_funcs_equal(self, ap, funcs, prob, tolerance=1e-10)  
+        assert_funcs_equal(self, ap, funcs, prob, tolerance=2e-10)  
 
     def test9(self): 
         from tests.test9 import setup_cb, gridFile, ap
