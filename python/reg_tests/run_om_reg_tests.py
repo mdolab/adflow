6e42be3cd8974aba49cd803f194d98008281cb6d
Justin Gray <justin.s.gray@gmail.com>
2018-03-31 20:53:46 -0400

    all test working now

--- python/reg_tests/run_om_reg_tests.py
+++ python/reg_tests/run_om_reg_tests.py
@@ -1,5 +1,5 @@
-# from om_tests.test_std_compare import Tests as StdTests
-# from om_tests.test_sol_compare import Tests as SolTests
+from om_tests.test_std_compare import Tests as StdTests
+from om_tests.test_sol_compare import Tests as SolTests
 from om_tests.test_adjoint_compare import Tests as AdjointTests
 
 if __name__ == "__main__": 
