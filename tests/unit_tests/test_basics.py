b1ddeac4230283effaa13482582a187f854788b8
joanibal <joanibal@umich.edu>
2020-04-13 16:12:18 -0400

    added better way to run all tests

--- tests/unit_tests/test_basics.py
+++ tests/unit_tests/test_basics.py
@@ -2,10 +2,17 @@ from __future__ import print_function
 import unittest
 import numpy
 
+import os
+import sys
+baseDir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(baseDir,'../../'))
+from python.pyADflow import ADFLOW
+
+
 class BasicTests(unittest.TestCase):
+    N_PROCS = 1
 
     def test_import(self):
-        from adflow import ADFLOW
         gridFile = 'input_files/mdo_tutorial_euler.cgns'
         options = {'gridfile': gridFile}
-        CFDSolver = ADFLOW(options=options)
+        CFDSolver = ADFLOW(options=options, debug=False)
