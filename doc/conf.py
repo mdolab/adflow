a692bec40f2832407ba7f5cbef801aebc6123b8b
Neil Wu <neilwu@umich.edu>
2020-10-22 10:58:28 -0400

    updated conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,19 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# This file only contains a selection of the most common options. For a full
-# list see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+from sphinx_mdolab_theme.config import * 
+import subprocess
 
-# -- Path setup --------------------------------------------------------------
-
+# -- Path setup -------------------------------------------------------------- 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 import os
 import sys
-import subprocess
 sys.path.insert(0, os.path.abspath("../"))
+
 # build doxygen
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 if read_the_docs_build:
@@ -21,44 +17,12 @@ if read_the_docs_build:
 
 # -- Project information -----------------------------------------------------
 project = 'ADflow'
-copyright = '2020, MDO Lab'
 
 # -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
-extensions = ['sphinx.ext.viewcode', 'sphinx.ext.imgmath', 'sphinx.ext.autodoc', 'numpydoc']
+# Built-in Sphinx extensions are already contained in the imported variable
+# here we add external extensions, which must also be added to requirements.txt
+# so that RTD can import and use them
+extensions.extend(["numpydoc"])
 
 # mock import for autodoc
 autodoc_mock_imports = ['numpy', 'mpi4py', 'petsc4py', 'baseclasses']
-
-numpydoc_show_class_members = False
-# The master toctree document.
-master_doc = 'index'
-
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
-html_context = {
-    'css_files': [
-        '_static/theme_overrides.css',  # override wide tables in RTD theme
-    ],
-}
