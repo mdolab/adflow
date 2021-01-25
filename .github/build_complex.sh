633fb37b49099f5465c0ccd4bf96e7870d347b0e
Bernardo Pacini <bpacini@umich.edu>
2021-01-25 10:32:15 -0500

    Adding `set -e` and fixing bash variable references.

--- .github/build_complex.sh
+++ .github/build_complex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 cp $CONFIG_FILE config/config.mk
-make -f Makefile_CS PETSC_ARCH=complex-opt-\$COMPILERS-\$PETSCVERSION
+make -f Makefile_CS PETSC_ARCH=complex-opt-$COMPILERS-$PETSCVERSION
 pip install .
