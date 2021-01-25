53fce6624b7dce88ff87d1f5171e9abd3eef2ece
Bernardo Pacini <bpacini@umich.edu>
2021-01-25 10:55:43 -0500

    Updated input files script call to not change directory.

--- .github/test_complex.sh
+++ .github/test_complex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-cd input_files && ./get-input-files.sh
+./input_files/get-input-files.sh
 export PETSC_ARCH=complex-opt-$COMPILERS-$PETSCVERSION
 testflo -v . -m "cmplx_test*" -n 1
