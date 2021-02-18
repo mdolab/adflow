6666e9822c063ac6de72f242719eedd297aa8f32
Marco Mangano <36549388+marcomangano@users.noreply.github.com>
2021-02-18 19:36:32 +0100

    revert to original bash - updated tarbomb

--- input_files/get-input-files.sh
+++ input_files/get-input-files.sh
@@ -4,4 +4,4 @@
 
 DIR=$(dirname $0)
 wget -O $DIR/input_files.tar.gz http://umich.edu/~mdolaboratory/repo_files/ADflow/adflow_input_files.tar.gz
-tar -xzf $DIR/input_files.tar.gz -C $DIR
+tar -xzf $DIR/input_files.tar.gz -C $DIR/../
