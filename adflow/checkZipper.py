ace7abf3d5d97c224b84ad7218dd922c1b975e9b
Neil Wu <neilwu@umich.edu>
2021-04-05 18:41:27 -0400

    made the same fix in complex scripts

--- adflow/checkZipper.py
+++ adflow/checkZipper.py
@@ -16,8 +16,8 @@ def checkZipper(fileName, options={}):
     """Run the zipper code on the supplied zipper debug file"""
 
     # Import the adflow module, we won't be useing much of it
-    curDir = os.path.dirname(os.path.realpath(__file__))
-    adflow = MExt.MExt("libadflow", [curDir], debug=True)._module
+    curDir = os.path.basename(os.path.dirname(os.path.realpath(__file__)))
+    adflow = MExt.MExt("libadflow", curDir, debug=True)._module
 
     # There is only 1 zipper-specific option
     adflow.inputoverset.selfzipcutoff = options.pop("selfZipCutoff", 120.0)
