c2e554f0c93c4cc5ef4d3c672e4ba2325fe1b510
Marco Mangano <36549388+marcomangano@users.noreply.github.com>
2022-04-13 18:22:11 -0400

    attempting tigher tolerances for complex tests

--- tests/reg_tests/test_adjoint.py
+++ tests/reg_tests/test_adjoint.py
@@ -335,10 +335,7 @@ class TestCmplxStep(reg_test_classes.CmplxRegTest):
             # this will happen when training, but will hopefully be fixed down the line
             return
 
-        if self.name == "Rotating_wing":
-            rtol = 2e-6
-        else:
-            rtol = 1e-8
+        rtol = 1e-8
         atol = 5e-10
 
         for dv in ["alpha", "mach"]:  # defaultAeroDVs:
@@ -380,10 +377,7 @@ class TestCmplxStep(reg_test_classes.CmplxRegTest):
 
         xRef = {"twist": [0.0] * 6, "span": [0.0], "shape": numpy.zeros(72, dtype="D")}
 
-        if self.name == "Rotating_wing":
-            rtol = 4e-6
-        else:
-            rtol = 5e-9
+        rtol = 5e-9
         atol = 5e-9
 
         for dv in ["span", "twist", "shape"]:
