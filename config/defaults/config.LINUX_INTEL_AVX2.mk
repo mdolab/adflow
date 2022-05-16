ca12ac9cee12c97ae13f40058e54e4c22da7331b
Anil Yildirim <anily@umich.edu>
2022-05-16 00:25:12 -0400

    new config file to do the avx2 stuff by default

---
+++ config/defaults/config.LINUX_INTEL_AVX2.mk
@@ -0,0 +1,53 @@
+# ----------------------------------------------------------------------
+# Config file for Intel ifort
+# ----------------------------------------------------------------------
+
+# ------- Define a possible parallel make (use PMAKE = make otherwise)--
+PMAKE = make -j 4
+
+# ------- Define the MPI Compilers--------------------------------------
+FF90 = mpiifort
+CC   = mpiicc
+
+# ------- Define Precision Flags ---------------------------------------
+# Options for Integer precision flags: -DUSE_LONG_INT
+# Options for Real precision flags: -DUSE_SINGLE_PRECISION, -DUSE_QUADRUPLE_PRECISION
+# Default (nothing specified) is 4 byte integers and 8 byte reals
+
+FF90_INTEGER_PRECISION_FLAG =
+FF90_REAL_PRECISION_FLAG    =
+CC_INTEGER_PRECISION_FLAG   =
+CC_REAL_PRECISION_FLAG      =
+
+# ------- Define CGNS Inlcude and linker flags -------------------------
+# Define the CGNS include directory and linking flags for the CGNS library.
+# We are assuming that HDF5 came from PETSc so it is included in ${PETSC_LIB}.
+# Otherwise you will have to specify the HDF5 library.
+CGNS_INCLUDE_FLAGS=-I$(CGNS_HOME)/include
+CGNS_LINKER_FLAGS=-L$(CGNS_HOME)/lib -lcgns
+
+# ------- Define Compiler Flags ----------------------------------------
+FF90_FLAGS   = -DHAS_ISNAN -fPIC -r8 -O2 -g -xCORE-AVX2
+C_FLAGS      = -DHAS_ISNAN -O -fPIC -xCORE-AVX2
+
+# ------- Define Archiver  and Flags -----------------------------------
+AR       = ar
+AR_FLAGS = -rvs
+
+# ------- Define Linker Flags ------------------------------------------
+LINKER       = $(FF90)
+LINKER_FLAGS = -nofor_main
+
+# ------- Define Petsc Info ---
+include ${PETSC_DIR}/lib/petsc/conf/variables
+PETSC_INCLUDE_FLAGS=${PETSC_CC_INCLUDES} -I$(PETSC_DIR)
+PETSC_LINKER_FLAGS=${PETSC_LIB}
+
+# Combine flags from above -- don't modify here
+FF90_PRECISION_FLAGS = $(FF90_INTEGER_PRECISION_FLAG)$(FF90_REAL_PRECISION_FLAG)
+CC_PRECISION_FLAGS   = $(CC_INTEGER_PRECISION_FLAG) $(CC_REAL_PRECISION_FLAG)
+
+# Define potentially different python, python-config and f2py executables:
+PYTHON = python
+PYTHON-CONFIG = python3-config # use python-config for python 2
+F2PY = f2py
