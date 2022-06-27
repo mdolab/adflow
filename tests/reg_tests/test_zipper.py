7ae6407cb0f199a34c11b607c1df5185c03315d0
sseraj <sseraj@umich.edu>
2022-06-27 18:23:36 -0400

    added overset update tests

--- tests/reg_tests/test_zipper.py
+++ tests/reg_tests/test_zipper.py
@@ -1,14 +1,16 @@
 import unittest
 import os
 import copy
+from parameterized import parameterized_class
 from adflow import ADFLOW
-from reg_default_options import adflowDefOpts
+from reg_default_options import adflowDefOpts, IDWarpDefOpts
 from reg_aeroproblems import ap_simple_cart_cube
-
+from idwarp import USMesh
+from pygeo import DVGeometry
 
 baseDir = os.path.dirname(os.path.abspath(__file__))
 
-# Tests for zipper meshes using a mesh with two overlapping cubes
+# Tests for overset and zipper meshes using a mesh with two overlapping cubes
 
 gridFile = os.path.join(baseDir, "../../input_files/cube_overset.cgns")
 commonTestOptions = {
@@ -25,7 +27,29 @@ commonTestOptions = {
     "nearWallDist": 1.0,
 }
 
+test_params = [
+    {
+        "name": "frozen",
+        "options": {
+            "oversetUpdateMode": "frozen",
+        },
+    },
+    {
+        "name": "fast",
+        "options": {
+            "oversetUpdateMode": "fast",
+        },
+    },
+    {
+        "name": "full",
+        "options": {
+            "oversetUpdateMode": "full",
+        },
+    },
+]
 
+
+@parameterized_class(test_params)
 class TestCubeOverset(unittest.TestCase):
     N_PROCS = 2
 
@@ -42,11 +66,31 @@ class TestCubeOverset(unittest.TestCase):
         # These are the modified options common to these tests
         options.update(commonTestOptions)
 
+        # Add the parameterized options
+        options.update(self.options)
+
         # Define the AeroProblem
         self.ap = copy.deepcopy(ap_simple_cart_cube)
 
         # Create the solver
         self.CFDSolver = ADFLOW(options=options, debug=False)
+
+        # Create mesh warping object
+        meshOptions = copy.copy(IDWarpDefOpts)
+        meshOptions.update({"gridFile": options["gridfile"]})
+        self.CFDSolver.setMesh(USMesh(options=meshOptions))
+
+        # Create geometry object
+        ffdFile = os.path.join(baseDir, "../../input_files/cube_overset_ffd.xyz")
+        DVGeo = DVGeometry(ffdFile)
+        DVGeo.addLocalDV("shape", lower=-0.5, upper=0.5, axis="z")
+        self.CFDSolver.setDVGeo(DVGeo)
+
+        # Apply shape change
+        dvDict = self.CFDSolver.DVGeo.getValues()
+        dvDict["shape"][4] = 0.1
+        self.CFDSolver.setAeroProblem(self.ap)
+        self.CFDSolver.DVGeo.setDesignVars(dvDict)
 
     def test_convergence(self):
 
