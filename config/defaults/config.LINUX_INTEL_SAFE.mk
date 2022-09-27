cdbd7e25fe4a9f0c1dc7537dd9af94120733bd74
Eirikur Jonsson <eirikurj@umich.edu>
2022-09-27 13:30:26 +0000

    Removing debug flags from config and unused flags

--- config/defaults/config.LINUX_INTEL_SAFE.mk
+++ config/defaults/config.LINUX_INTEL_SAFE.mk
@@ -31,9 +31,9 @@ CGNS_INCLUDE_FLAGS=-I$(CGNS_HOME)/include
 CGNS_LINKER_FLAGS=-L$(CGNS_HOME)/lib -lcgns
 
 # ------- Define Compiler Flags ----------------------------------------
-FF77_FLAGS   = -DHAS_ISNAN -fPIC -r8 -O1 -g
+FF77_FLAGS   = -fPIC -r8 -O1
 FF90_FLAGS = $(FF77_FLAGS) -std08
-C_FLAGS      = -DHAS_ISNAN -O -fPIC
+C_FLAGS      = -fPIC -O
 
 # ------- Define Archiver  and Flags -----------------------------------
 AR       = ar
