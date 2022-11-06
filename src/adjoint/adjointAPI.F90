83a611d46bc96130eac88954afb6fbb2b703564f
Neil Wu <602725+nwu63@users.noreply.github.com>
2022-11-06 12:44:25 -0500

    Compile with `-std=f2008` (#228)
    * updated make rule for F77 and F90 with std=f2008
    
    * fixed tabs
    
    * swapping around variable declaration orders
    
    * use myisnan instead of non-standard isnan function from GCC
    
    * fixed some string and format issues
    
    * fixed some warnings
    
    * fix real/complex types
    
    * Fixing type issues for t0
    
    * Fixing previously fixed string continuation
    
    * Update obsolete string definitions
    
    * Update myisnan to handle different types and to use more modern modules. Removing older c function
    
    * More fixes
    
    * Remove old Makefile
    
    * Update intel fortran flags
    
    * More fixes for the intel compiler
    
    * Updating linker flags
    
    * Make new module compile without optimization
    
    * Add new file
    
    * Removing debug flags from config and unused flags
    
    * Remove line continuation
    
    * Updating build system build certain files without optimization
    
    * get myisnan from the new module
    
    * Updating build files for complex
    
    * ran tapenade
    
    * remove whitespace
    
    Co-authored-by: Eirikur Jonsson <36180221+eirikurj@users.noreply.github.com>
    Co-authored-by: Sabet Seraj <48863473+sseraj@users.noreply.github.com>

--- src/adjoint/adjointAPI.F90
+++ src/adjoint/adjointAPI.F90
@@ -233,7 +233,7 @@ contains
     ! of the fact that only wbar needs to be zeroed since all other
     ! required seeds are zeroed in the individual fast routines. This is
     ! slightly unsafe, but it necessary for speed.
-    
+
     ! Allocate the memory for reverse
     if (.not. derivVarsAllocated) then
       call allocDerivativeValues(level)
@@ -275,10 +275,10 @@ contains
     implicit none
 
     ! Input Variables
+    integer(kind=intType), intent(in) :: nDOF
     real(kind=realType), dimension(ndof), intent(in) :: inVec
     real(kind=realType), dimension(ndof), intent(out) :: outVec
     real(kind=realType), intent(in) :: relativeTolerance
-    integer(kind=intType), intent(in) :: nDOF
     integer(kind=intTYpe) :: adjointConvergedReason
     ! Working variables
     integer(kind=intType) :: ierr, nn, sps
@@ -360,10 +360,10 @@ contains
     implicit none
 
     ! Input Variables
+    integer(kind=intType), intent(in) :: nDOF
     real(kind=realType), dimension(ndof), intent(in) :: inVec
     real(kind=realType), dimension(ndof), intent(out) :: outVec
     real(kind=realType), intent(in) :: relativeTolerance
-    integer(kind=intType), intent(in) :: nDOF
     integer(kind=intTYpe) :: adjointConvergedReason
     ! Working variables
     integer(kind=intType) :: ierr, nn, sps
@@ -440,7 +440,7 @@ contains
     implicit none
 
     ! Input params
-    character*(*), intent(in) :: fileName
+    character(len=*), intent(in) :: fileName
 
     ! Working parameters
     PetscViewer binViewer
@@ -468,7 +468,7 @@ contains
     implicit none
 
     ! Input params
-    character*(*), intent(in) :: fileName
+    character(len=*), intent(in) :: fileName
 
     ! Working parameters
     PetscViewer binViewer
@@ -496,9 +496,9 @@ contains
     implicit none
 
     ! Input params
-    character*(*), intent(in) :: fileName
-    real(kind=realType), dimension(nState) :: RHS
+    character(len=*), intent(in) :: fileName
     integer(kind=intType) :: nstate
+    real(kind=realType), dimension(nState) :: RHS
 
     ! Working parameters
     PetscViewer binViewer
@@ -697,8 +697,8 @@ contains
     implicit none
 
     ! Input Parameters
-    real(kind=realType), dimension(nState) :: RHS, psi
     integer(kind=intType) :: nState
+    real(kind=realType), dimension(nState) :: RHS, psi
     logical :: checkSolution
     !
     !     Local variables.
@@ -966,7 +966,7 @@ contains
     implicit none
 
     ! Input params
-    character*(*), intent(in) :: fileName
+    character(len=*), intent(in) :: fileName
 
     ! Working parameters
     PetscViewer binViewer
