bc4dc8648ea4d35336d899bac6349745f2175181
sseraj <sseraj@umich.edu>
2022-12-21 10:46:04 -0500

    updated documentation in format module

--- src/modules/format.F90
+++ src/modules/format.F90
@@ -1,12 +1,23 @@
 module format
 !
 !       This module contains common format specifiers for printing output.
+!       The convention for adding a format variable in this module or elsewhere is as follows:
+!
+!       commonFormats module variable: formats spanning multiple modules
+!       other module variable: formats spanning multiple subroutines within a module
+!       subroutine variable: formats that are repeated within a subroutine
+!       string: one-off cases
 !
        use constants, only : maxStringLen
        implicit none
        save
 
-       ! An arbitrary number of strings
+       ! The * in these patterns means that it is applied as often as there are input arguments matching this pattern.
+       ! For example, (*(A, 1X)) means that this formatting pattern is applied N times if you have N strings that
+       ! you want to be separated by a space.
+       ! Similarly, (*(A, ES12.5)) expects N (string, float) pairs as an input.
+
+       ! Strings
        character(len=maxStringLen) :: strings = '(*(A))'
 
        ! Strings followed by one space
