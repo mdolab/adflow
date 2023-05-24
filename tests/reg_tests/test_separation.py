28f0a8095b007c6c933bd6fea590111bdf4f8c42
arshsaja <arshsaja@umich.edu>
2023-05-24 10:55:05 -0400

    complex test

--- tests/reg_tests/test_separation.py
+++ tests/reg_tests/test_separation.py
@@ -6,6 +6,7 @@ import copy
 
 # MACH classes
 from adflow import ADFLOW
+from adflow import ADFLOW_C
 
 # import the testing utilities
 from reg_default_options import adflowDefOpts
@@ -135,6 +136,141 @@ class SeparationBasicTests(reg_test_classes.RegTest):
             dotLocal2 = fDot_xv[funcName] / self.CFDSolver.comm.size
             self.handler.par_add_sum(f"Dot product test for xV -> {funcName}", dotLocal1, rtol=5e-10)
             self.handler.par_add_sum(f"Dot product test for xV -> {funcName}", dotLocal2, rtol=5e-10, compare=True)
+
+
+class SeparationCmplxTests(reg_test_classes.CmplxRegTest):
+    """
+    Complex step tests for the separation functions.
+    """
+
+    N_PROCS = 2
+    ref_file = "separation_tests.json"
+    h = 1e-40
+
+    def setUp(self):
+        super().setUp()
+
+        gridFile = os.path.join(baseDir, "../../input_files/naca0012_L3_SEP.cgns")
+
+        options = copy.copy(adflowDefOpts)
+        options.update(
+            {
+                "gridfile": gridFile,
+                "outputdirectory": os.path.join(baseDir, "../output_files"),
+                "writevolumesolution": False,
+                "writesurfacesolution": False,
+                "writetecplotsurfacesolution": False,
+                "mgcycle": "sg",
+                "ncycles": 1000,
+                "useanksolver": True,
+                "usenksolver": True,
+                "anksecondordswitchtol": 1e-2,
+                "nkswitchtol": 1e-6,
+                "volumevariables": ["temp", "mach", "resrho", "cp"],
+                "equationType": "RANS",
+                "l2convergence": 1e-13,
+                "adjointl2convergence": 1e-13,
+                # to get slightly better complex convergence
+                "NKUseEW": False,
+                "NKLinearSolveTol": 1e-4,
+                "sepSensorModel": "surfvec",
+                "sepSweepAngleCorrection": 0.0,
+            }
+        )
+
+        # Setup aeroproblem
+        self.ap = copy.copy(ap_naca0012_separation)
+        # change the name
+        self.ap.name = "naca0012_rans_2D"
+
+        # add aoa DV
+        self.ap.addDV("alpha", name="alpha")
+
+        CFDSolver = ADFLOW_C(options=options, debug=True)
+
+        self.CFDSolver = CFDSolver
+
+        self.CFDSolver.addFamilyGroup("wingup", ["upper_skin"])
+
+        self.CFDSolver.addFunction("sepsensor", "wingup")
+
+    def cmplx_test_separation_adjoints(self):
+        aDV = {"alpha": self.ap.alpha}
+        funcs = {}
+        funcsSensCS = {}
+
+        # run the baseline evaluation
+        self.CFDSolver(self.ap)
+        # check if solution failed
+        self.assert_solution_failure()
+        self.CFDSolver.evalFunctions(self.ap, funcs)
+
+        evalFuncs = ["sepsensor_wingup", "cl"]
+        funcs_plus = {}
+        for dv in ["alpha", "vol_perturbation"]:
+            if dv == "alpha":
+                # save the old alpha
+                dvsave = aDV["alpha"]
+                # perturb
+                aDV["alpha"] += self.h * 1j
+                self.ap.setDesignVars(aDV)
+            elif dv == "vol_perturbation":
+                xVDot = self.CFDSolver.getSpatialPerturbation(314)
+                # gridSave = np.zeros_like(xVDot)
+                # get the original grid
+                gridSave = self.CFDSolver.adflow.warping.getgrid(self.CFDSolver.getSpatialSize())
+                # perturb using the random seed
+                # this is very intrusive but it works and its testing these functions sensitivities
+                # wrt geometric DVs w/o actually having a mesh or dvgeo object
+                self.CFDSolver.adflow.warping.setgrid(gridSave + self.h * 1j * xVDot)
+                self.CFDSolver.adflow.preprocessingapi.updatecoordinatesalllevels()
+                self.CFDSolver.adflow.walldistance.updatewalldistancealllevels()
+                self.CFDSolver.adflow.preprocessingapi.updatemetricsalllevels()
+                self.CFDSolver.adflow.preprocessingapi.updategridvelocitiesalllevels()
+
+            # call solver again
+            # we can also not re-set the flow and call the solver a few times until complex parts converge
+            self.CFDSolver.resetFlow(self.ap)
+            self.CFDSolver(self.ap)  # the complex residuals dont converge well for the coordinate perturbation
+
+            # check if solution failed
+            self.assert_solution_failure()
+
+            # save the new funcs in a dict
+            funcs_plus[dv] = {}
+
+            # eval functions
+            self.CFDSolver.evalFunctions(self.ap, funcs_plus[dv])
+
+            # compute the sens
+            funcsSensCS[dv] = {}
+            for f in evalFuncs:
+                funcsSensCS[dv][self.ap[f]] = np.imag(funcs_plus[dv][self.ap[f]]) / self.h
+
+            # reset the DV
+            if dv == "alpha":
+                aDV["alpha"] = dvsave
+                self.ap.setDesignVars(aDV)
+            elif dv == "vol_perturbation":
+                # set the original grid back
+                self.CFDSolver.adflow.warping.setgrid(gridSave)
+
+        ##################
+        # TEST DERIVATIVES
+        ##################
+
+        # here we compare the CS derivatives to the adjoint values computed by the real test
+        # we treat the CS value as the truth, so if this test passes,
+        # we assume the adjoint sensitivities are also true
+
+        for funcName in evalFuncs:
+            fullName = f"naca0012_rans_2D_{funcName}"
+
+            refVal = self.handler.db["separation totals"][fullName]["alpha"]
+            np.testing.assert_allclose(funcsSensCS["alpha"][fullName], refVal, atol=1e-10, rtol=1e-10)
+
+            refVal = self.handler.db[f"total {funcName} derivative wrt random volume perturbation"]
+            np.testing.assert_allclose(funcsSensCS["vol_perturbation"][fullName], refVal, rtol=1e-3)
 
 
 if __name__ == "__main__":
