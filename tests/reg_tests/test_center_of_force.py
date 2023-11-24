79a290aa57be33cb37538df46009de8b895e4c4d
Anil Yildirim <anily@umich.edu>
2023-11-24 14:36:35 +0100

    update test

--- tests/reg_tests/test_center_of_force.py
+++ tests/reg_tests/test_center_of_force.py
@@ -13,34 +13,6 @@ from reg_aeroproblems import ap_tutorial_wing
 
 baseDir = os.path.dirname(os.path.abspath(__file__))
 
-def get_rot_mat(vx, vy, vz, theta):
-    # function to get the rotation matrix
-    # this is copied over from cgnsutils
-    # Normalize the components of the rotation vector
-    normV = np.sqrt(vx**2 + vy**2 + vz**2)
-    uu = vx / normV
-    vv = vy / normV
-    ww = vz / normV
-
-    # Compute sines and cosines of the rotation angle
-    ss = np.sin(theta * np.pi / 180.0)
-    cc = np.cos(theta * np.pi / 180.0)
-
-    # Build rotation matrix
-    rotMat = np.zeros((3, 3))
-    rotMat[0, 0] = uu * uu + (1.0 - uu * uu) * cc
-    rotMat[0, 1] = uu * vv * (1.0 - cc) - ww * ss
-    rotMat[0, 2] = uu * ww * (1.0 - cc) + vv * ss
-    rotMat[1, 0] = uu * vv * (1.0 - cc) + ww * ss
-    rotMat[1, 1] = vv * vv + (1.0 - vv * vv) * cc
-    rotMat[1, 2] = vv * ww * (1.0 - cc) - uu * ss
-    rotMat[2, 0] = uu * ww * (1.0 - cc) - vv * ss
-    rotMat[2, 1] = vv * ww * (1.0 - cc) + uu * ss
-    rotMat[2, 2] = ww * ww + (1.0 - ww * ww) * cc
-
-    return rotMat
-
-
 class TestCenterOfForce(unittest.TestCase):
     """
     Test center of force outputs' consistency with moment outputs,
