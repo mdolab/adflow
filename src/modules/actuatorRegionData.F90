20cba1f1cbc32f18039e0132dddc82f9b11b50a7
Shamsheer S. Chauhan <sschau@umich.edu>
2024-04-17 16:44:07 -0400

    Modify actuatorRegionData.F90 for the propeller model

--- src/modules/actuatorRegionData.F90
+++ src/modules/actuatorRegionData.F90
@@ -3,30 +3,50 @@ module actuatorRegionData
 
     type actuatorRegionType
 
-        character(len=maxStringLen) :: famName
+        character(len=maxStringLen) :: famName, actType
         integer(kind=intType) :: famID
         ! The block indexes of the cells included in this region
         integer(kind=intType), dimension(:, :), pointer :: cellIDs
 
+        ! The tangent unit-vectors of the cells included in this region
+        real(kind=realType), dimension(:, :), pointer :: cellTangentials
+        ! The radii of the cells included in this region
+        real(kind=realType), dimension(:), pointer :: cellRadii
+
         ! The total number of cells included this proc has
         integer(kind=intType) :: nCellIDs
 
-        ! the force vector to be applied on this region
-        ! this is equal to torque * axisVec
-        real(kind=realType) :: force(3)
+        ! F is the total Force to be applied on this region
+        real(kind=realType) :: F(3)
 
-        ! magnitude of the total torque to be applied on this region
-        real(kind=realType) :: torque
-        ! vector that determines the direction of the applied torque
+        ! thrust is the total thrust magnitude to be applied on this region
+        real(kind=realType) :: thrust
+        real(kind=realType) :: swirlFact
+        real(kind=realType) :: mDistribParam
+        real(kind=realType) :: nDistribParam
+        real(kind=realType) :: distribPDfactor
+        real(kind=realType) :: innerZeroThrustRadius
+        real(kind=realType) :: spinnerRadius
+        real(kind=realType) :: rootDragFactor
+
+        ! T is the total torque to be applied on this regoin
+        real(kind=realType) :: T
         real(kind=realType), dimension(3) :: axisVec
+        real(kind=realType), dimension(:, :), pointer :: thrustVec
+        real(kind=realType), dimension(:, :), pointer :: swirlVec
 
         ! total heat flux to be added on this regoin
-        real(kind=realType) :: heat
+        ! Shamsheer note: heat is a new variable in the latest mdolab ADflow
+        ! real(kind=realType) :: heat
 
         ! Volume is the total integrated volume of all cells (on all
         ! procs) included in this region
         real(kind=realType) :: volume
-        real(kind=realType) :: volLocal
+        ! Shamsheer note: volLocal is a new variable in the latest mdolab ADflow
+        ! real(kind=realType) :: volLocal
+
+        real(kind=realType) :: totalThrustSum
+        real(kind=realType) :: totalSwirlSum
 
         integer(kind=intType), dimension(:), allocatable :: blkPtr
 
