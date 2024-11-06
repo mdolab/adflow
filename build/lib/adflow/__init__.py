a474ab3946e2a215b98024ad8b4ca09198dfa9bb
Safa <bakhshi@umich.edu>
2024-11-06 15:38:02 -0500

    Remove extra files

--- build/lib/adflow/__init__.py
+++
@@ -1,8 +0,0 @@
-__version__ = "2.10.0"
-
-from mpi4py import MPI
-
-from .pyADflow import ADFLOW
-from .pyADflow_C import ADFLOW_C
-from .oversetCheck import OversetCheck
-from .checkZipper import checkZipper
