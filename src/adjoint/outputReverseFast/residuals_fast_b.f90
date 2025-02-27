6ce5859e477cf9b5f1bd6bd0bcdd9e35109997fe
Eirikur Jonsson <eirikurj@umich.edu>
2025-02-27 14:38:09 +0000

    update AD files

--- src/adjoint/outputReverseFast/residuals_fast_b.f90
+++ src/adjoint/outputReverseFast/residuals_fast_b.f90
@@ -362,11 +362,17 @@ contains
 ! compute the relaxation factor based on the ordersconverged
 ! how far we are into the ramp:
     if (ordersconverged .lt. actuatorregions(iregion)%relaxstart) then
+myIntPtr = myIntPtr + 1
+ myIntStack(myIntPtr) = 0
       factor = zero
     else if (ordersconverged .gt. actuatorregions(iregion)%relaxend) &
 &   then
+myIntPtr = myIntPtr + 1
+ myIntStack(myIntPtr) = 1
       factor = one
     else
+myIntPtr = myIntPtr + 1
+ myIntStack(myIntPtr) = 1
 ! in between
       ostart = actuatorregions(iregion)%relaxstart
       oend = actuatorregions(iregion)%relaxend
@@ -375,9 +381,10 @@ contains
 ! if using the uniform force distribution
     if (actuatorregions(iregion)%acttype .eq. 'uniform') then
 ! compute the constant force factor
-    f_fact = factor*actuatorregions(iregion)%f/actuatorregions(&
-&     iregion)%volume/pref
+      f_fact = factor*actuatorregions(iregion)%force/actuatorregions(&
+&       iregion)%volume/pref
     end if
+! heat factor. this is heat added per unit volume per unit time
 ! loop over the ranges for this block
     istart = actuatorregions(iregion)%blkptr(nn-1) + 1
     iend = actuatorregions(iregion)%blkptr(nn)
@@ -391,6 +398,7 @@ myIntPtr = myIntPtr + 1
     end if
 ! if using the simple propeller force distribution
     if (actuatorregions(iregion)%acttype .eq. 'simpleprop') then
+!$bwd-of ii-loop 
       do ii=istart,iend
 ! extract the cell id.
         i = actuatorregions(iregion)%cellids(1, ii)
@@ -418,28 +426,30 @@ branch = myIntStack(myIntPtr)
  myIntPtr = myIntPtr - 1
     if (branch .eq. 0) then
 !$bwd-of ii-loop 
-    do ii=istart,iend
+      do ii=istart,iend
 ! extract the cell id.
-      i = actuatorregions(iregion)%cellids(1, ii)
-      j = actuatorregions(iregion)%cellids(2, ii)
-      k = actuatorregions(iregion)%cellids(3, ii)
+        i = actuatorregions(iregion)%cellids(1, ii)
+        j = actuatorregions(iregion)%cellids(2, ii)
+        k = actuatorregions(iregion)%cellids(3, ii)
 ! this actually gets the force
-      ftmp = vol(i, j, k)*f_fact
+        ftmp = vol(i, j, k)*f_fact
 ! this gets the heat addition rate
-      if (res) then
-        vxd = -(ftmp(1)*dwd(i, j, k, irhoe))
-        vyd = -(ftmp(2)*dwd(i, j, k, irhoe))
-        vzd = -(ftmp(3)*dwd(i, j, k, irhoe))
-      else
-        vxd = 0.0_8
-        vyd = 0.0_8
-        vzd = 0.0_8
-      end if
-      wd(i, j, k, ivz) = wd(i, j, k, ivz) + vzd
-      wd(i, j, k, ivy) = wd(i, j, k, ivy) + vyd
-      wd(i, j, k, ivx) = wd(i, j, k, ivx) + vxd
-    end do
+        if (res) then
+          vxd = -(ftmp(1)*dwd(i, j, k, irhoe))
+          vyd = -(ftmp(2)*dwd(i, j, k, irhoe))
+          vzd = -(ftmp(3)*dwd(i, j, k, irhoe))
+        else
+          vxd = 0.0_8
+          vyd = 0.0_8
+          vzd = 0.0_8
+        end if
+        wd(i, j, k, ivz) = wd(i, j, k, ivz) + vzd
+        wd(i, j, k, ivy) = wd(i, j, k, ivy) + vyd
+        wd(i, j, k, ivx) = wd(i, j, k, ivx) + vxd
+      end do
     end if
+branch = myIntStack(myIntPtr)
+ myIntPtr = myIntPtr - 1
   end subroutine sourceterms_block_fast_b
 
   subroutine sourceterms_block(nn, res, iregion, plocal)
@@ -477,44 +487,45 @@ branch = myIntStack(myIntPtr)
 ! if using the uniform force distribution
     if (actuatorregions(iregion)%acttype .eq. 'uniform') then
 ! compute the constant force factor
-    f_fact = factor*actuatorregions(iregion)%f/actuatorregions(&
-&     iregion)%volume/pref
+      f_fact = factor*actuatorregions(iregion)%force/actuatorregions(&
+&       iregion)%volume/pref
+    end if
 ! heat factor. this is heat added per unit volume per unit time
     q_fact = factor*actuatorregions(iregion)%heat/actuatorregions(&
 &     iregion)%volume/(pref*uref*lref*lref)
-    end if
 ! loop over the ranges for this block
     istart = actuatorregions(iregion)%blkptr(nn-1) + 1
     iend = actuatorregions(iregion)%blkptr(nn)
-!$ad ii-loop
 ! if using the uniform force distribution
     if (actuatorregions(iregion)%acttype .eq. 'uniform') then
-    do ii=istart,iend
+!$ad ii-loop
+      do ii=istart,iend
 ! extract the cell id.
-      i = actuatorregions(iregion)%cellids(1, ii)
-      j = actuatorregions(iregion)%cellids(2, ii)
-      k = actuatorregions(iregion)%cellids(3, ii)
+        i = actuatorregions(iregion)%cellids(1, ii)
+        j = actuatorregions(iregion)%cellids(2, ii)
+        k = actuatorregions(iregion)%cellids(3, ii)
 ! this actually gets the force
-      ftmp = vol(i, j, k)*f_fact
-      vx = w(i, j, k, ivx)
-      vy = w(i, j, k, ivy)
-      vz = w(i, j, k, ivz)
+        ftmp = vol(i, j, k)*f_fact
+        vx = w(i, j, k, ivx)
+        vy = w(i, j, k, ivy)
+        vz = w(i, j, k, ivz)
 ! this gets the heat addition rate
-      qtmp = vol(i, j, k)*q_fact
-      if (res) then
+        qtmp = vol(i, j, k)*q_fact
+        if (res) then
 ! momentum residuals
-        dw(i, j, k, imx:imz) = dw(i, j, k, imx:imz) - ftmp
+          dw(i, j, k, imx:imz) = dw(i, j, k, imx:imz) - ftmp
 ! energy residuals
-        dw(i, j, k, irhoe) = dw(i, j, k, irhoe) - ftmp(1)*vx - ftmp(2)*&
-&         vy - ftmp(3)*vz - qtmp
-      else
+          dw(i, j, k, irhoe) = dw(i, j, k, irhoe) - ftmp(1)*vx - ftmp(2)&
+&           *vy - ftmp(3)*vz - qtmp
+        else
 ! add in the local power contribution:
-        plocal = plocal + (vx*ftmp(1)+vy*ftmp(2)+vz*ftmp(3))*redim
-      end if
-    end do
+          plocal = plocal + (vx*ftmp(1)+vy*ftmp(2)+vz*ftmp(3))*redim
+        end if
+      end do
     end if
 ! if using the simple propeller force distribution
     if (actuatorregions(iregion)%acttype .eq. 'simpleprop') then
+!$ad ii-loop
       do ii=istart,iend
 ! extract the cell id.
         i = actuatorregions(iregion)%cellids(1, ii)
